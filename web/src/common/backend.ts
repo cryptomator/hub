2de2e92c085845d07384cafb7413b02c09358c84
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2021-08-05 08:52:17 +0200

    allow cross-origin requests during dev

--- web/src/common/backend.ts
+++ web/src/common/backend.ts
@@ -1,13 +1,15 @@
 import axios, { AxiosResponse } from 'axios';
 import auth from './auth';
 
+const REST_BASE = import.meta.env.DEV ? 'http://localhost:9090' : '';
+
 class VaultService {
 
   public async createVault(uuid: string, name: string, masterkey: String, iterations: number, salt: String): Promise<AxiosResponse<any>> {
     if (!auth.isAuthenticated()) {
       return Promise.reject('not logged in');
     }
-    return axios.put('/vaults/' + uuid, { name: name, masterkey: masterkey, iterations: iterations, salt: salt }, {
+    return axios.put(REST_BASE + '/vaults/' + uuid, { name: name, masterkey: masterkey, iterations: iterations, salt: salt }, {
       headers: {
         'Authorization': 'Bearer ' + auth.bearerToken()
       }
