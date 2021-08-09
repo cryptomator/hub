9d31729e259538a7dad21860bfbc7f594a5acdc2
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2021-08-09 10:26:57 +0200

    allow user interaction during certain states of the unlock workflow

--- web/src/components/UnlockVault.vue
+++ web/src/components/UnlockVault.vue
@@ -1,11 +1,33 @@
 <template>
-  <h1>Unlock vault</h1>
+  <h1>Unlock {{ vaultId }}</h1>
+
+  <div v-if="state === State.Processing">
+    TODO: show spinner?
+  </div>
+  <div v-else-if="state === State.Unlocked">
+    Vault unlocked successfully. You may close this window now.
+  </div>
+  <div v-else-if="state === State.ChangedPublicKey">
+    This device reports a new public key. Please ask your admin to remove it.
+  </div>
+  <div v-else-if="state === State.NewDevice">
+    This is the first time you try to unlock this vault.
+    <input v-model="deviceName" type="text" placeholder="Device Name">
+    <button @click="createDevice()">Request Access</button>
+  </div>
 </template>
 
 <script lang="ts">
 import backend from '../common/backend'
 import { defineComponent } from 'vue'
 import { AxiosError } from 'axios';
+
+enum State {
+  Processing,
+  ChangedPublicKey,
+  NewDevice,
+  Unlocked,
+}
 
 export default defineComponent({
   name: 'Other',
@@ -27,29 +49,42 @@ export default defineComponent({
         default: null
     }
   },
+  data: () => ({
+    State,
+    state: State.Processing as State,
+    deviceName: '' as string,
+  }),
   mounted() {
     backend.devices.getDevice(this.deviceId).then(device => {
       if(device.data.publicKey === this.deviceKey) {
         this.getKeyForThisVault()
       } else {
-        // FIXME the fingerprint of the key is equal but the public key not, may don't check this?
+        this.$data.state = State.ChangedPublicKey; // TODO redirect CANCELLED
       }
-    }).catch((reason: AxiosError) => {
-      if (reason.response?.status === 404) {
-        this.createDevice()
+    }).catch((error: AxiosError) => {
+      if (error.response?.status === 404) {
+        this.$data.state = State.NewDevice; // TODO redirect CANCELLED
       } else {
-        console.log(reason.message)
+        throw error; // TODO redirect CANCELLED(/ERROR?)
       }
-  })
+    });
   },
   methods: {
     async createDevice() {
-      await backend.devices.createDevice(this.deviceId, "name", this.deviceKey)
-      this.getKeyForThisVault()
+      await backend.devices.createDevice(this.deviceId, this.$data.deviceName, this.deviceKey)
     },
-    async getKeyForThisVault() {
-     // const publicKey = await backend.vaults.unlock(this.vaultId, this.deviceId)
-     // window.location.href = this.redirectTo
+    getKeyForThisVault() {
+      backend.vaults.getKeyFor(this.vaultId, this.deviceId)
+      .then(publicKey => {
+        this.$data.state = State.Unlocked; // TODO redirect SUCCESS
+      })
+      .catch((error: AxiosError) => {
+        if (error.response?.status === 404) {
+          console.warn('device not authorized'); // TODO redirect UNAUTHORIZED
+        } else {
+          throw error; // TODO redirect CANCELLED(/ERROR?)
+        }
+      });
     }
   }
 })
