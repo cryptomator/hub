72445de76fdaf5dcfb8a3f05822782b3e8d87109
Martin Beyer <martin.beyer@skymatic.de>
2021-08-31 23:31:15 +0200

    Add basic navBar (from Cryptomator.org)

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+	<Navbar v-show="showNavbar" />
   <ul>
     <li><router-link to="/">Home</router-link></li>
     <li><router-link to="/vaults/create">Create vault</router-link></li>
@@ -8,6 +9,26 @@
   <router-view/>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue';
+
+export default {
+	name: 'App',
+	components: {
+		Navbar,
+		//Footer,
+	},
+	computed: {
+		showNavbar() {
+			return this.$route.meta.navbar == null ? true : this.$route.meta.navbar;
+		},
+		/*showFooter() {
+			return this.$route.meta.footer == null ? true : this.$route.meta.footer;
+		},*/
+	},
+};
+</script>
+
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,6 +36,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
---
+++ web/src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="w-full text-primary lg:px-8 border-t-2 border-primary shadow ">
+    <div>
+      <!-- main menu -->
+      <div class="flex justify-between items-stretch w-full">
+        <a class="flex items-center hover:no-underline py-2 mx-2 lg:ml-0" href="/">
+          <img
+            src="../assets/logo.svg"
+            class="h-8"
+            alt="Logo"
+          >
+          <span class="font-headline font-bold ml-2 pb-px">CRYPTOMATOR HUB</span>
+        </a>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+	name: "Navbar.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
