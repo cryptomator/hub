a5d38391725929a5416c6dd104e577b4e2299465
Armin Schrenk <armin.schrenk@skymatic.de>
2021-10-25 13:17:52 +0200

    make hub config persistence synchronously

--- spi/src/main/java/org/cryptomator/hub/config/HubConfigWriter.java
+++ spi/src/main/java/org/cryptomator/hub/config/HubConfigWriter.java
@@ -1,5 +1,7 @@
 package org.cryptomator.hub.config;
 
+import org.jboss.logging.Logger;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,53 +15,43 @@ import java.util.concurrent.atomic.AtomicReference;
 
 class HubConfigWriter {
 
-    private final Path configPath;
-    private final ExecutorService executorService;
+	private static final Logger LOG = Logger.getLogger(HubConfigWriter.class);
+	private static final String HUB_CONFIG_DESCRIPTION = "This file contains configuration values for Crypotmator Hub.";
 
-    private AtomicReference<Properties> nextJob;
+	private final Path configPath;
+	private final ExecutorService executorService;
 
-    public HubConfigWriter(Path configPath) {
-        this.configPath = configPath;
-        this.executorService = Executors.newSingleThreadExecutor();
-        this.nextJob = new AtomicReference<>(null);
-    }
 
-    synchronized void persist(Map<String, String> config) {
-        var prop = new Properties();
-        prop.putAll(config);
-        nextJob.set(prop);
-        executorService.submit(this::write);
-    }
+	private AtomicReference<Properties> nextJob;
 
-    private Runnable write() {
-        return () -> {
-            Properties prop;
-            synchronized (this) {
-                prop = nextJob.getAndSet(null);
-            }
-            if (prop != null) {
-                try (var out = Files.newOutputStream(configPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-                    prop.store(out, "TODO");
-                } catch (IOException e) {
-                    //TODO: LOG
-                    e.printStackTrace();
-                }
-            }
-        };
-    }
+	public HubConfigWriter(Path configPath) {
+		this.configPath = configPath;
+		this.executorService = Executors.newSingleThreadExecutor();
+		this.nextJob = new AtomicReference<>(null);
+	}
 
-    Map<String, String> read() {
-        Map<String, String> config = new HashMap<>();
-        try (var in = Files.newInputStream(configPath, StandardOpenOption.READ)) {
-            var prop = new Properties();
-            prop.load(in);
-            prop.forEach((k, v) -> config.put((String) k, (String) v));
-        } catch (IOException e) {
-            //TODO: Log
-            e.printStackTrace();
-        }
-        return config;
-    }
+	synchronized void persist(Map<String, String> config) {
+		var prop = new Properties();
+		prop.putAll(config);
+		try (var out = Files.newOutputStream(configPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+			prop.store(out, HUB_CONFIG_DESCRIPTION);
+		} catch (IOException e) {
+			LOG.warn("Unable to persist hub config to {}.", configPath, e);
+		}
+	}
+
+
+	Map<String, String> read() {
+		Map<String, String> config = new HashMap<>();
+		try (var in = Files.newInputStream(configPath, StandardOpenOption.READ)) {
+			var prop = new Properties();
+			prop.load(in);
+			prop.forEach((k, v) -> config.put((String) k, (String) v));
+		} catch (IOException e) {
+			LOG.info("Unable to read hub config from {}, creating empty.", configPath, e);
+		}
+		return config;
+	}
 
 
 }
