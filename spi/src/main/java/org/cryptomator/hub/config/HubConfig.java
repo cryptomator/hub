7a04616e8f5a6b2ae9d0d1c988a59a2c2b3f430f
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2021-10-28 13:56:55 +0200

    formatted

--- spi/src/main/java/org/cryptomator/hub/config/HubConfig.java
+++ spi/src/main/java/org/cryptomator/hub/config/HubConfig.java
@@ -13,78 +13,78 @@ import java.util.stream.StreamSupport;
 
 public class HubConfig implements ConfigSource {
 
-    private static final Logger LOG = Logger.getLogger(HubConfig.class);
-    private static final String HUB_CONFIGSOURCE_NAME = "HubConfig";
-    private static final String HUB_CONFIGPATH_PROPERTY_KEY = "hub.config.path";
+	private static final Logger LOG = Logger.getLogger(HubConfig.class);
+	private static final String HUB_CONFIGSOURCE_NAME = "HubConfig";
+	private static final String HUB_CONFIGPATH_PROPERTY_KEY = "hub.config.path";
 
-    private final HubConfigPersistence persistence;
-    private final Properties config;
+	private final HubConfigPersistence persistence;
+	private final Properties config;
 
-    public HubConfig() {
-        var configPath = System.getProperty(HUB_CONFIGPATH_PROPERTY_KEY);
-        if (configPath == null) {
-            throw new IllegalStateException("Property " + HUB_CONFIGPATH_PROPERTY_KEY + " not set.");
-        }
+	public HubConfig() {
+		var configPath = System.getProperty(HUB_CONFIGPATH_PROPERTY_KEY);
+		if (configPath == null) {
+			throw new IllegalStateException("Property " + HUB_CONFIGPATH_PROPERTY_KEY + " not set.");
+		}
 
-        LOG.info("Hub config persists to " + configPath);
-        this.config = new Properties();
-        this.persistence = new HubConfigPersistence(Path.of(configPath));
+		LOG.info("Hub config persists to " + configPath);
+		this.config = new Properties();
+		this.persistence = new HubConfigPersistence(Path.of(configPath));
 
-        persistence.load(config);
-    }
+		persistence.load(config);
+	}
 
-    @Override
-    public Set<String> getPropertyNames() {
-        return config.stringPropertyNames();
-    }
+	@Override
+	public Set<String> getPropertyNames() {
+		return config.stringPropertyNames();
+	}
 
-    @Override
-    // for default sources and their precedence see https://quarkus.io/guides/config-reference#configuration-sources
-    public int getOrdinal() {
-        return 270;
-    }
+	@Override
+	// for default sources and their precedence see https://quarkus.io/guides/config-reference#configuration-sources
+	public int getOrdinal() {
+		return 270;
+	}
 
-    @Override
-    public String getValue(String key) {
-        return config.getProperty(key);
-    }
+	@Override
+	public String getValue(String key) {
+		return config.getProperty(key);
+	}
 
-    @Override
-    public String getName() {
-        return HUB_CONFIGSOURCE_NAME;
-    }
+	@Override
+	public String getName() {
+		return HUB_CONFIGSOURCE_NAME;
+	}
 
-    public synchronized void setProperty(Key key, String value) {
-        String oldVal = (String) config.setProperty(key.toString(), value);
-        if (oldVal == null || !oldVal.equals(value)) {
-            persistence.persist(config);
-        }
-    }
+	public synchronized void setProperty(Key key, String value) {
+		String oldVal = (String) config.setProperty(key.toString(), value);
+		if (oldVal == null || !oldVal.equals(value)) {
+			persistence.persist(config);
+		}
+	}
 
-    @Produces
-    @Named("HubConfig")
-    public static HubConfig getInstance() {
-        return (HubConfig) StreamSupport.stream(ConfigProvider.getConfig().getConfigSources().spliterator(), false)
-                .filter(s -> s instanceof HubConfig)
-                .findFirst()
-                .get();
-    }
+	@Produces
+	@Named("HubConfig")
+	public static HubConfig getInstance() {
+		return (HubConfig) StreamSupport.stream(ConfigProvider.getConfig().getConfigSources().spliterator(), false)
+				.filter(s -> s instanceof HubConfig)
+				.findFirst()
+				.get();
+	}
 
-    public enum Key {
+	public enum Key {
 
-        //TODO: replace by real property key
-        DUMMY_VALUE("dummy.value");
+		//TODO: replace by real property key
+		DUMMY_VALUE("dummy.value");
 
-        private final String actualKeyString;
+		private final String actualKeyString;
 
-        Key(String actualKeyString) {
-            this.actualKeyString = actualKeyString;
-        }
+		Key(String actualKeyString) {
+			this.actualKeyString = actualKeyString;
+		}
 
-        @Override
-        public String toString() {
-            return actualKeyString;
-        }
-    }
+		@Override
+		public String toString() {
+			return actualKeyString;
+		}
+	}
 
 }
--- spi/src/main/java/org/cryptomator/hub/config/HubConfigPersistence.java
+++ spi/src/main/java/org/cryptomator/hub/config/HubConfigPersistence.java
@@ -12,32 +12,32 @@ import java.util.Properties;
 
 class HubConfigPersistence {
 
-    private static final Logger LOG = Logger.getLogger(HubConfigPersistence.class);
-    private static final String HUB_CONFIG_DESCRIPTION = "Cryptomator Hub configuration persistence file";
+	private static final Logger LOG = Logger.getLogger(HubConfigPersistence.class);
+	private static final String HUB_CONFIG_DESCRIPTION = "Cryptomator Hub configuration persistence file";
 
-    private final Path configPath;
+	private final Path configPath;
 
-    HubConfigPersistence(Path configPath) {
-        this.configPath = configPath;
-    }
+	HubConfigPersistence(Path configPath) {
+		this.configPath = configPath;
+	}
 
-    synchronized void persist(Properties properties) {
-        try (var out = Files.newOutputStream(configPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
-            properties.store(out, HUB_CONFIG_DESCRIPTION);
-        } catch (IOException e) {
-            throw new UncheckedIOException("Unable to persist Hub config to " + configPath, e);
-        }
-    }
+	synchronized void persist(Properties properties) {
+		try (var out = Files.newOutputStream(configPath, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+			properties.store(out, HUB_CONFIG_DESCRIPTION);
+		} catch (IOException e) {
+			throw new UncheckedIOException("Unable to persist Hub config to " + configPath, e);
+		}
+	}
 
-    synchronized void load(Properties prop) {
-        try (var in = Files.newInputStream(configPath, StandardOpenOption.READ)) {
-            prop.load(in);
-        } catch (NoSuchFileException e) {
-            LOG.debugf("No hub config file found at %s.", configPath);
-        } catch (IOException e) {
-            LOG.warn(String.format("Unable to read hub config from %s, creating empty.", configPath), e);
-        }
-    }
+	synchronized void load(Properties prop) {
+		try (var in = Files.newInputStream(configPath, StandardOpenOption.READ)) {
+			prop.load(in);
+		} catch (NoSuchFileException e) {
+			LOG.debugf("No hub config file found at %s.", configPath);
+		} catch (IOException e) {
+			LOG.warn(String.format("Unable to read hub config from %s, creating empty.", configPath), e);
+		}
+	}
 
 
 }
