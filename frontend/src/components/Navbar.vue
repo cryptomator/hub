7fdd3c50b967a3e0f70e8a887fda82bd3f8522f4
Tobias Hagemann <tobias.hagemann@skymatic.de>
2021-11-04 14:43:28 +0100

    fixed and cleaned up navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -5,27 +5,20 @@
         <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
           <!-- Mobile menu button-->
           <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="sr-only">
-              Open main menu</span>
+            <span class="sr-only">Open main menu</span>
             <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
             <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
           </DisclosureButton>
         </div>
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex-shrink-0 flex items-center">
-            <img
-              src="../assets/logo.svg"
-              class="h-8"
-              alt="Logo"
-            >
+            <img src="../assets/logo.svg" class="h-8" alt="Logo"/>
             <span class="font-headline font-bold text-primary ml-2 pb-px">CRYPTOMATOR HUB</span>
-            <!--<img class="block lg:hidden h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow" />
-            <img class="hidden lg:block h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow" />-->
           </div>
           <div class="hidden sm:block sm:ml-6">
             <div class="flex space-x-4">
-              <router-link v-for="item in navigation" :key="item.name" :v-if="!item.to === '/logout' && isLoggedIn()" :to="item.to"
-                           class="text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium'"
+              <router-link v-for="item in navigation" :key="item.name" :to="item.to"
+                           class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                            active-class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
                            exact-path
               >
@@ -41,6 +34,7 @@
             </option>
           </select>
         </div>
+
         <!-- Profile dropdown -->
         <Menu as="div" class="ml-3 relative">
           <div>
@@ -50,30 +44,26 @@
             </MenuButton>
           </div>
           <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
-            <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white divide-y divide-gray-100 ring-1 ring-black ring-opacity-5 focus:outline-none">
               <MenuItem v-slot="{ active }">
-                <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
-                  ?Your connected Devices?
-                </a>
+                <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Your Devices</a>
               </MenuItem>
-              <MenuItem v-if="isLoggedIn()" v-slot="{ active }" @click="logout()">
-                <a :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
-                  Log out
-                </a>
-              </MenuItem>
-              <MenuItem v-if="!isLoggedIn()" v-slot="{ active }" @click="logout()">
-                <a :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">
-                  Logged out
-                </a>
+              <MenuItem v-slot="{ active }">
+                <a :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']" @click="logout()">Sign out</a>
               </MenuItem>
             </MenuItems>
           </transition>
         </Menu>
       </div>
     </div>
+
     <DisclosurePanel class="sm:hidden">
       <div class="px-2 pt-2 pb-3 space-y-1">
-        <router-link v-for="item in navigation" :key="item.name" :to="item.to" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">
+        <router-link v-for="item in navigation" :key="item.name" :to="item.to"
+                     class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                     active-class="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
+                     exact-path
+        >
           {{ t(item.name) }}
         </router-link>
       </div>
@@ -93,14 +83,12 @@ const navigation = [
   { name: 'home', to: '/'},
   { name: "create_vault_title", to: '/vaults/create'},
   { name: 'user_details', to: '/user'},
-  { name: 'logout', to: '/logout'},
-  { name: 'setup', to: "/setup"}
 ]
 
 const languages = Locales
 
 export default defineComponent({
-  name: "Navbar.vue",
+  name: "Navbar",
   components: {
     Disclosure,
     DisclosureButton,
@@ -110,9 +98,9 @@ export default defineComponent({
     MenuItem,
     MenuItems,
     MenuIcon,
-	XIcon,
+	  XIcon,
   },
-  setup(){
+  setup() {
     const { t } = useI18n({
   	  useScope: 'global'
   	})
@@ -123,10 +111,6 @@ export default defineComponent({
     }
   },
   methods: {
-    async isLoggedIn(){
-      const auth = await authPromise;
-      return auth.isAuthenticated();
-    },
     async logout() {
       const auth = await authPromise;
       auth.logout();
@@ -135,7 +119,3 @@ export default defineComponent({
 
 })
 </script>
-
-<style scoped>
-
-</style>
