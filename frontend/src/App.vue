a4758a06cc5dbea15243b08f9a966a90445000a2
Tobias Hagemann <tobias.hagemann@skymatic.de>
2021-11-17 10:35:18 +0100

    added background color, refined paddings

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,14 @@
   <vue-notification-list position="top-right"></vue-notification-list>
   <Navbar v-show="showNavbar" />
 
-  <div class="my-5 max-w-7xl mx-auto sm:px-6 lg:px-8">
+  <div class="max-w-7xl mx-auto px-4 py-12 sm:px-6 lg:px-8">
     <router-view></router-view>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Navbar from './components/Navbar.vue';
-import VaultList from './components/VaultList.vue';
 import { useI18n } from 'vue-i18n';
-
 
 export default {
   name: 'App',
@@ -20,30 +18,18 @@ export default {
     //Footer,
   },
   setup() {
-  	const { t } = useI18n({
-  	  useScope: 'global'
-  	});
-  	return { t };
-  	},
+    const { t } = useI18n({
+      useScope: 'global'
+    });
+    return { t };
+  },
   computed: {
     showNavbar() {
       return this.$route.meta.navbar == null ? true : this.$route.meta.navbar;
     },
     /*showFooter() {
-			return this.$route.meta.footer == null ? true : this.$route.meta.footer;
-		},*/
+      return this.$route.meta.footer == null ? true : this.$route.meta.footer;
+    },*/
   },
 };
 </script>
-
-<style>
-#app {
-
-  /*font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 0px;*/
-}
-</style>
--- frontend/src/components/AddDevice.vue
+++ frontend/src/components/AddDevice.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="space-y-6 mt-12">
+  <div class="space-y-6">
     <div class="bg-white shadow px-4 py-5 sm:rounded-lg sm:p-6">
       <div class="md:grid md:grid-cols-3 md:gap-6">
         <div class="md:col-span-1">
