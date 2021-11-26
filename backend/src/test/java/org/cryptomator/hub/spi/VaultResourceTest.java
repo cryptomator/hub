bc7c54cc1e7b5e084f76212d383a074b69493028
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2021-11-26 11:01:40 +0100

    cleanup

--- backend/src/test/java/org/cryptomator/hub/spi/VaultResourceTest.java
+++ backend/src/test/java/org/cryptomator/hub/spi/VaultResourceTest.java
@@ -13,6 +13,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
 
 @QuarkusTest
 @DisplayName("Resource /vaults")
@@ -34,11 +35,8 @@ public class VaultResourceTest {
 		@Test
 		@DisplayName("GET /vaults/vault1/keys/device1 returns 200")
 		public void testUnlock1() {
-			given()
-					.when()
-					.get("/vaults/vault1/keys/device1")
-					.then()
-					.statusCode(200)
+			when().get("/vaults/vault1/keys/device1")
+					.then().statusCode(200)
 					.body("device_specific_masterkey", CoreMatchers.is("dsm1"))
 					.body("ephemeral_public_key", CoreMatchers.is("epk1"));
 		}
@@ -46,21 +44,15 @@ public class VaultResourceTest {
 		@Test
 		@DisplayName("GET /vaults/vault1/keys/noSuchDevice returns 404")
 		public void testUnlock2() {
-			given()
-					.when()
-					.get("/vaults/vault1/keys/noSuchDevice")
-					.then()
-					.statusCode(404);
+			when().get("/vaults/vault1/keys/noSuchDevice")
+					.then().statusCode(404);
 		}
 
 		@Test
 		@DisplayName("GET /vaults/vault1/keys/device2 returns 403")
 		public void testUnlock3() {
-			given()
-					.when()
-					.get("/vaults/vault1/keys/device2")
-					.then()
-					.statusCode(403);
+			when().get("/vaults/vault1/keys/device2")
+					.then().statusCode(403);
 		}
 
 	}
@@ -78,13 +70,9 @@ public class VaultResourceTest {
 		public void testCreateVault1() {
 			var vaultDto = new VaultResource.VaultDto("vault1", "My Vault", "masterkey3", "42", "NaCl");
 
-			given()
-					.when()
-					.contentType(ContentType.JSON)
-					.body(vaultDto)
-					.put("/vaults/vault1")
-					.then()
-					.statusCode(409);
+			given().contentType(ContentType.JSON).body(vaultDto)
+					.when().put("/vaults/{vaultId}", "vault1")
+					.then().statusCode(409);
 		}
 
 		@Test
@@ -92,13 +80,22 @@ public class VaultResourceTest {
 		public void testCreateVault2() {
 			var vaultDto = new VaultResource.VaultDto("vault3", "My Vault", "masterkey3", "42", "NaCl");
 
-			given()
-					.when()
-					.contentType(ContentType.JSON)
-					.body(vaultDto)
-					.put("/vaults/vault3")
-					.then()
-					.statusCode(201);
+			given().contentType(ContentType.JSON).body(vaultDto)
+					.when().put("/vaults/{vaultId}", "vault3")
+					.then().statusCode(201);
+		}
+
+	}
+
+	@Nested
+	@DisplayName("As unauthenticated user")
+	public class AsAnonymous {
+
+		@Test
+		@DisplayName("GET /vaults/vault1/keys/device1 returns 401")
+		public void testUnlock1() {
+			when().get("/vaults/vault1/keys/device1")
+					.then().statusCode(401);
 		}
 
 	}
