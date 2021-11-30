e0cd15298316dba7c99f1400a588518f33c6259f
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2021-11-30 16:55:29 +0100

    add integration tests for entity classes (references #23)

---
+++ backend/src/test/java/org/cryptomator/hub/entities/EntityIntegrationTest.java
@@ -0,0 +1,57 @@
+package org.cryptomator.hub.entities;
+
+import com.radcortez.flyway.test.annotation.DataSource;
+import com.radcortez.flyway.test.annotation.FlywayTest;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.transaction.Transactional;
+
+@QuarkusTest
+@FlywayTest(value = @DataSource(url = "jdbc:h2:mem:test"))
+@DisplayName("Persistent Entities")
+public class EntityIntegrationTest {
+
+	@Test
+	@DisplayName("Removing a Vault Member cascades to Access")
+	public void removeMemberCascadesToAccess() {
+		tx(() -> {
+			Vault vault = Vault.findById("vault1");
+			Assumptions.assumeTrue(vault.access.stream().anyMatch(a -> "user1".equals(a.user.id)));
+
+			vault.members.removeIf(m -> "user1".equals(m.id));
+			vault.persist();
+		});
+
+		tx(() -> {
+			User user = User.findById("user1");
+			Assertions.assertNotNull(user);
+
+			Vault vault = Vault.findById("vault1");
+			Assertions.assertFalse(vault.access.stream().anyMatch(a -> user.equals(a.user)));
+		});
+	}
+
+	@Test
+	@DisplayName("Removing a Device cascades to Access")
+	public void removingDeviceCascadesToAccess() {
+		tx(() -> {
+			Device device = Device.findById("device1");
+			device.delete();
+		});
+
+		tx(() -> {
+			boolean match = Access.<Access>findAll().stream().anyMatch(a -> "device1".equals(a.device.id));
+			Assertions.assertFalse(match);
+		});
+	}
+
+	@Transactional(Transactional.TxType.REQUIRES_NEW)
+	public static void tx(Runnable validation) {
+		validation.run();
+	}
+
+}
