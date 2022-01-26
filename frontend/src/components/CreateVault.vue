3ee4b2be4af3aaaa91668fcc346d61438f47f1f8
Armin Schrenk <armin.schrenk@skymatic.de>
2022-01-26 12:31:53 +0100

    error handling for createVault component

--- frontend/src/components/CreateVault.vue
+++ frontend/src/components/CreateVault.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="state == State.Initial || state == State.FormValidationFailed || state == State.Processing">
+  <div v-if="state == State.Initial || state == State.Processing">
     <form ref="form" novalidate @submit.prevent="createVault()">
       <div class="shadow sm:rounded-lg sm:overflow-hidden">
         <div class="bg-white px-4 py-5 sm:p-6">
@@ -17,12 +17,12 @@
               <div class="grid grid-cols-6 gap-6">
                 <div class="col-span-6 sm:col-span-3">
                   <label for="vaultName" class="block text-sm font-medium text-gray-700">{{ t('createVault.vaultName') }}</label>
-                  <input id="vaultName" v-model="vaultName" :disabled="state == State.Processing" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md disabled:bg-gray-200" :class="{ 'invalid:border-red-300 invalid:text-red-900 focus:invalid:ring-red-500 focus:invalid:border-red-500': state == State.FormValidationFailed }" required />
+                  <input id="vaultName" v-model="vaultName" :disabled="state == State.Processing" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md disabled:bg-gray-200" :class="{ 'invalid:border-red-300 invalid:text-red-900 focus:invalid:ring-red-500 focus:invalid:border-red-500': formValidationFailed }" required />
                 </div>
 
                 <div class="col-span-6 sm:col-span-4">
                   <label for="password" class="block text-sm font-medium text-gray-700">{{ t('createVault.masterPassword') }}</label>
-                  <input id="password" v-model="password" :disabled="state == State.Processing" type="password" minlength="8" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md disabled:bg-gray-200" :class="{ 'invalid:border-red-300 invalid:text-red-900 focus:invalid:ring-red-500 focus:invalid:border-red-500': state == State.FormValidationFailed }" aria-describedby="password-description" required />
+                  <input id="password" v-model="password" :disabled="state == State.Processing" type="password" minlength="8" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md disabled:bg-gray-200" :class="{ 'invalid:border-red-300 invalid:text-red-900 focus:invalid:ring-red-500 focus:invalid:border-red-500': formValidationFailed }" aria-describedby="password-description" required />
                   <p id="password-description" class="mt-2 text-sm text-gray-500">{{ t('createVault.masterPasssord.description') }}</p>
                 </div>
               </div>
@@ -31,7 +31,8 @@
         </div>
 
         <div class="flex justify-end items-center px-4 py-3 bg-gray-50 sm:px-6">
-          <p v-if="state == State.FormValidationFailed" class="text-sm text-red-900 mr-4">{{ t('createVault.formValidationFailed') }}</p>
+          <p v-if="formValidationFailed" class="text-sm text-red-900 mr-4">{{ t('createVault.formValidationFailed') }}</p>
+          <p v-if="vaultCreationFailed" class="text-sm text-red-900 mr-4">{{ t('createVault.vaultCreationFailed') }}</p>
           <button :disabled="state == State.Processing" type="submit" class="flex-none inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 disabled:hover:bg-primary disabled:cursor-not-allowed">
             {{ t('createVault.submit') }}
           </button>
@@ -62,6 +63,7 @@
               <DownloadIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
               {{ t('createVault.success.download') }}
             </button>
+            <p v-if="downloadTemplateFailed" class="text-sm text-red-900 mr-4">{{ t('createVault.downloadTemplateFailed') }}</p> <!-- TODO: not beautiful-->
           </div>
           <div class="mt-2">
             <router-link to="/" class="text-sm text-gray-500">
@@ -87,7 +89,6 @@ import { VaultConfig } from '../common/vaultconfig';
 
 enum State {
   Initial,
-  FormValidationFailed,
   Processing,
   Finished
 }
@@ -96,16 +97,23 @@ const { t } = useI18n({ useScope: 'global' });
 
 const form = ref<HTMLFormElement>();
 
+const formValidationFailed = ref(false);
+const vaultCreationFailed = ref(false);
+const downloadTemplateFailed = ref(false);
+
 const state = ref(State.Initial);
 const vaultName = ref('');
 const password = ref('');
 const vaultConfig = ref<VaultConfig>();
 
 async function createVault() {
-  if (!form.value?.checkValidity()) {
-    state.value = State.FormValidationFailed;
+  vaultCreationFailed.value = false;
+  formValidationFailed.value = !form.value?.checkValidity();
+
+  if (formValidationFailed.value) {
     return;
   }
+
   try {
     state.value = State.Processing;
     const vaultId = uuid();
@@ -115,19 +123,21 @@ async function createVault() {
     await backend.vaults.createVault(vaultId, vaultName.value, wrapped.encrypted, wrapped.iterations, wrapped.salt);
     state.value = State.Finished;
   } catch (error) {
-    // TODO: error handling
+    vaultCreationFailed.value = true;
     console.error('Creating vault failed.', error);
     state.value = State.Initial;
   }
+  return;
 }
 
 async function downloadVaultTemplate() {
+  downloadTemplateFailed.value = false;
   if (state.value === State.Finished) {
     const blob = await vaultConfig.value?.exportTemplate();
     if (blob != null) {
       saveAs(blob, `${vaultName.value}.zip`);
     } else {
-      // TODO: error handling
+      downloadTemplateFailed.value = true;
       console.error('Downloading vault template failed.');
     }
   }
