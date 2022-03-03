10a42dff8cc12a6f9659d72d614d826f50557637
Armin Schrenk <armin.schrenk@skymatic.de>
2022-03-03 13:13:08 +0100

    add optional text to label

--- frontend/src/components/CreateVault.vue
+++ frontend/src/components/CreateVault.vue
@@ -22,7 +22,9 @@
                 </div>
 
                 <div class="col-span-6 sm:col-span-4">
-                  <label for="vaultDescription" class="block text-sm font-medium text-gray-700">{{ t('createVault.vaultDescription') }}</label>
+                  <label for="vaultDescription" class="block text-sm font-medium text-gray-700">
+                    {{ t('createVault.vaultDescription') }}
+                    <span class="text-xs text-gray-500">({{ t('common.optional') }})</span></label>
                   <input id="vaultDescription" v-model="vaultDescription" :disabled="state == State.Processing" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md disabled:bg-gray-200"/>
                 </div>
 
