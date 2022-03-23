8bd2e45dab6225d169077ff985946b96c8b10958
Tobias Hagemann <tobias.hagemann@skymatic.de>
2022-03-23 17:40:50 +0100

    Added license validation, fixes #54

--- backend/src/main/java/org/cryptomator/hub/api/BillingResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/BillingResource.java
@@ -1,9 +1,14 @@
 package org.cryptomator.hub.api;
 
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.cryptomator.hub.entities.Billing;
+import org.cryptomator.hub.license.LicenseValidator;
 import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -16,21 +21,36 @@ import javax.ws.rs.core.Response;
 @Path("/billing")
 public class BillingResource {
 
+	@Inject
+	LicenseValidator licenseValidator;
+
 	@GET
 	@Path("/")
+	@RolesAllowed("admin")
 	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
 	@Operation(summary = "get the billing information")
+	@APIResponse(responseCode = "200")
+	@APIResponse(responseCode = "403", description = "only admins are allowed to get the billing information")
 	public BillingDto get() {
 		return Billing.<Billing>findByIdOptional(0).map(BillingDto::fromEntity).get();
 	}
 
 	@PUT
 	@Path("/token")
+	@RolesAllowed("admin")
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Transactional
 	@Operation(summary = "set the token")
+	@APIResponse(responseCode = "204")
+	@APIResponse(responseCode = "400", description = "token is invalid (e.g., expired or invalid signature)")
+	@APIResponse(responseCode = "403", description = "only admins are allowed to set the token")
 	public Response setToken(String token) {
+		try {
+			licenseValidator.validate(token);
+		} catch (JWTVerificationException e) {
+			return Response.status(Response.Status.BAD_REQUEST).build();
+		}
 		var billing = Billing.<Billing>findByIdOptional(0).get();
 		billing.token = token;
 		billing.persist();
---
+++ backend/src/test/java/org/cryptomator/hub/api/BillingResourceTest.java
@@ -0,0 +1,143 @@
+package org.cryptomator.hub.api;
+
+import com.radcortez.flyway.test.annotation.DataSource;
+import com.radcortez.flyway.test.annotation.FlywayTest;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.security.TestSecurity;
+import io.quarkus.test.security.oidc.Claim;
+import io.quarkus.test.security.oidc.OidcSecurity;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+@FlywayTest(value = @DataSource(url = "jdbc:h2:mem:test"), additionalLocations = {"classpath:org/cryptomator/hub/flyway"})
+@DisplayName("Resource /billing")
+class BillingResourceTest {
+
+	@BeforeAll
+	public static void beforeAll() {
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+	}
+
+	@Nested
+	@DisplayName("As admin")
+	@FlywayTest(value = @DataSource(url = "jdbc:h2:mem:test"), additionalLocations = {"classpath:org/cryptomator/hub/flyway"}, clean = false)
+	@TestSecurity(user = "Admin", roles = {"admin"})
+	@OidcSecurity(claims = {
+			@Claim(key = "sub", value = "admin")
+	})
+	@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+	public class AsAdmin {
+
+		private static final String INITIAL_TOKEN = "eyJhbGciOiJFUzUxMiJ9.eyJqdGkiOiI0MiIsImlhdCI6MTY0ODA0OTM2MCwiaXNzIjoiU2t5bWF0aWMiLCJhdWQiOiJDcnlwdG9tYXRvciBIdWIiLCJzdWIiOiJodWJAY3J5cHRvbWF0b3Iub3JnIiwic2VhdHMiOjUsImV4cCI6MjUzNDAyMjE0NDAwLCJyZWZyZXNoVXJsIjoiaHR0cDovL2xvY2FsaG9zdDo4Nzg3L2h1Yi9zdWJzY3JpcHRpb24_aHViX2lkPTQyIn0.AKyoZ0WQ8xhs8vPymWPHCsc6ch6pZpfxBcrF5QjVLSQVnYz2s5QF3nnkwn4AGR7V14TuhkJMZLUZxMdQAYLyL95sAV2Fu0E4-e1v3IVKlNKtze89eqYvEs6Ak9jWjtecOgPWNWjz2itI4MfJBDmbFtTnehOtqRqUdsDoC9NFik2C7tHm";
+		private static final String UPDATED_TOKEN = "eyJhbGciOiJFUzUxMiJ9.eyJqdGkiOiI0MiIsImlhdCI6MTY0ODA1MDIxMCwiaXNzIjoiU2t5bWF0aWMiLCJhdWQiOiJDcnlwdG9tYXRvciBIdWIiLCJzdWIiOiJodWJAY3J5cHRvbWF0b3Iub3JnIiwic2VhdHMiOjUsImV4cCI6MjUzNDAyMjE0NDAwLCJyZWZyZXNoVXJsIjoiaHR0cDovL2xvY2FsaG9zdDo4Nzg3L2h1Yi9zdWJzY3JpcHRpb24_aHViX2lkPTQyIn0.AfYO7yp2-HpfT3yAfhX-2Hc-y4POPRevUNhW9IrL0Ru4BuwJCdf7KLuYIQFeNQ8Etz2RIg6rAnoVJ-xuP4RNE7RmAJuVhrYOyxHmF6btj3e3ES9JyBJW15Yw4R9iM3LKycNxL5OXsktC7I--_IOrB70C05511uGoifbKvvrnbapfn8gE";
+
+		@Test
+		@Order(1)
+		@DisplayName("GET /billing returns 200")
+		public void testGet() {
+			when().get("/billing")
+					.then().statusCode(200)
+					.body("hub_id", is("42"))
+					.body("token", is(INITIAL_TOKEN));
+		}
+
+		@Test
+		@Order(2)
+		@DisplayName("PUT /billing/token returns 204")
+		public void testPutValidToken() {
+			given().contentType(ContentType.TEXT).body(UPDATED_TOKEN)
+					.when().put("/billing/token")
+					.then().statusCode(204);
+		}
+
+		@Test
+		@Order(3)
+		@DisplayName("GET /billing returns 200 with updated token")
+		public void testGetUpdated() {
+			when().get("/billing")
+					.then().statusCode(200)
+					.body("hub_id", is("42"))
+					.body("token", is(UPDATED_TOKEN));
+		}
+
+		@Test
+		@Order(4)
+		@DisplayName("PUT /billing/token returns 400 due to expired token")
+		public void testPutExpiredToken() {
+			given().contentType(ContentType.TEXT).body("eyJhbGciOiJFUzUxMiJ9.eyJqdGkiOiI0MiIsImlhdCI6MTY0ODA1MTA0MCwiaXNzIjoiU2t5bWF0aWMiLCJhdWQiOiJDcnlwdG9tYXRvciBIdWIiLCJzdWIiOiJodWJAY3J5cHRvbWF0b3Iub3JnIiwic2VhdHMiOjUsImV4cCI6LTYyMTY3MjE5MjAwLCJyZWZyZXNoVXJsIjoiaHR0cDovL2xvY2FsaG9zdDo4Nzg3L2h1Yi9zdWJzY3JpcHRpb24_aHViX2lkPTQyIn0.AK9Du5MNsyVgOzicgi2S7ECxxqZPBLP8cFByAAZ7_y96NEvrwOiR8NNmlZlfvebMfckaYUEg-nf3BAd1JHAxur1UADyYbKSLoMs4B69SkbKW0drjfY9RjFUhO_w6sS4gg39_X_IhbwW6wRkGFoqGRI0juaCPViQqV5WFIcj7RbuCcNJ6")
+					.when().put("/billing/token")
+					.then().statusCode(400);
+		}
+
+		@Test
+		@Order(5)
+		@DisplayName("PUT /billing/token returns 400 due to invalid signature")
+		public void testPutTokenWithInvalidSignature() {
+			given().contentType(ContentType.TEXT).body("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+					.when().put("/billing/token")
+					.then().statusCode(400);
+		}
+
+		@Test
+		@Order(6)
+		@DisplayName("PUT /billing/token returns 400 due to malformed token")
+		public void testPutMalformedToken() {
+			given().contentType(ContentType.TEXT).body("hello world")
+					.when().put("/billing/token")
+					.then().statusCode(400);
+		}
+
+	}
+
+	@Nested
+	@DisplayName("As any other role")
+	@TestSecurity(user = "User Name 1", roles = {"user", "vault-owner"})
+	@OidcSecurity(claims = {
+			@Claim(key = "sub", value = "user1")
+	})
+	public class AsAnyOtherRole {
+
+		@DisplayName("403 Forbidden")
+		@ParameterizedTest(name = "{0} {1}")
+		@CsvSource(value = {
+				"GET, /billing",
+//				"PUT, /billing/token"
+		})
+		public void testGet(String method, String path) {
+			when().request(method, path)
+					.then().statusCode(403);
+		}
+
+	}
+
+	@Nested
+	@DisplayName("As unauthenticated user")
+	public class AsAnonymous {
+
+		@DisplayName("401 Unauthorized")
+		@ParameterizedTest(name = "{0} {1}")
+		@CsvSource(value = {
+				"GET, /billing",
+//				"PUT, /billing/token"
+		})
+		public void testGet(String method, String path) {
+			when().request(method, path)
+					.then().statusCode(401);
+		}
+
+	}
+}
