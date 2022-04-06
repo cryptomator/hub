3d18d259f8744a8b2e1fe194c4100b99a0261017
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2022-04-06 18:20:54 +0200

    new DDL allowing users and groups to (recursively) be members of other groups

--- backend/src/main/resources/org/cryptomator/hub/flyway/V1.0.0__Initial_Setup.sql
+++ backend/src/main/resources/org/cryptomator/hub/flyway/V1.0.0__Initial_Setup.sql
@@ -5,60 +5,115 @@ CREATE TABLE "billing"
     "id"     INT4 NOT NULL,
     "hub_id" VARCHAR(255) NOT NULL,
     "token"  VARCHAR(2000),
-    CONSTRAINT "PK_BILLING" PRIMARY KEY ("id")
+    CONSTRAINT "BILLING_PK" PRIMARY KEY ("id")
 );
 
-CREATE TABLE "user"
+CREATE TABLE "authority"
 (
 	"id"          VARCHAR(255) NOT NULL,
+	"type"        VARCHAR(5) NOT NULL,
 	"name"        VARCHAR(255) NOT NULL,
+	CONSTRAINT "AUTHORITY_PK" PRIMARY KEY ("id", "type"),
+	CONSTRAINT "AUTHORITY_CHK_TYPE" CHECK ("type" = 'user' OR "type" = 'group')
+);
+
+CREATE TABLE "group_membership"
+(
+	"group_id"          VARCHAR(255) NOT NULL,
+	"group_type"        VARCHAR(5) NOT NULL,
+	"member_id"         VARCHAR(255) NOT NULL,
+    "member_type"       VARCHAR(5) NOT NULL,
+    CONSTRAINT "GROUP_MEMBERSHIP_PK" PRIMARY KEY ("group_id", "group_type", "member_id", "member_type"),
+	CONSTRAINT "GROUP_MEMBERSHIP_FK_GROUP" FOREIGN KEY ("group_id", "group_type") REFERENCES "authority" ("id", "type") ON DELETE CASCADE,
+	CONSTRAINT "GROUP_MEMBERSHIP_FK_MEMBER" FOREIGN KEY ("member_id", "member_type") REFERENCES "authority" ("id", "type") ON DELETE CASCADE,
+	CONSTRAINT "GROUP_MEMBERSHIP_CHK_TYPE" CHECK ("group_type" = 'group')
+);
+
+CREATE VIEW "effective_group_membership" ("group_id", "member_id", "member_type") AS
+WITH RECURSIVE "members" ("group_id", "member_id", "member_type") AS (
+	SELECT "gm"."group_id", "gm"."member_id", "gm"."member_type" FROM "group_membership" "gm"
+	UNION
+	SELECT "mem"."group_id", "gm"."member_id", "gm"."member_type" FROM "group_membership" "gm"
+		INNER JOIN "members" "mem" ON "gm"."group_id" = "mem"."member_id" AND "gm"."group_type" = "mem"."member_type"
+) SELECT * FROM "members";
+
+-- demo daten
+INSERT INTO "authority" (id, type, name) VALUES
+	('1', 'group', 'Group1'),
+	('2', 'group', 'Group2'),
+	('1', 'user', 'User1'),
+	('2', 'user', 'User2'),
+	('3', 'user', 'User3'),
+	('4', 'user', 'User4');
+
+INSERT INTO "group_membership" ("group_id", "group_type", "member_id", "member_type") VALUES
+	('1', 'group', '2', 'group'),
+	('1', 'group', '1', 'user'),
+	('2', 'group', '2', 'user'),
+	('1', 'group', '3', 'user'),
+	('2', 'group', '3', 'user');
+-- end demo daten
+
+CREATE TABLE "user_details"
+(
+	"id"          VARCHAR(255) NOT NULL,
+	"type"        VARCHAR(5) NOT NULL,
 	"picture_url" VARCHAR(255),
 	"email"       VARCHAR(255),
-	CONSTRAINT "PK_USER" PRIMARY KEY ("id")
+    CONSTRAINT "USER_DETAIL_PK" PRIMARY KEY ("id", "type"),
+	CONSTRAINT "USER_DETAIL_FK_USER" FOREIGN KEY ("id", "type") REFERENCES "authority" ("id", "type") ON DELETE CASCADE,
+	CONSTRAINT "USER_DETAIL_CHK_TYPE" CHECK ("type" = 'user')
 );
 
 CREATE TABLE "vault"
 (
 	"id"            VARCHAR(255) NOT NULL,
-	"user_id"       VARCHAR(255) NOT NULL,
+	"owner"         VARCHAR(255) NOT NULL,
+	"owner_type"    VARCHAR(5) NOT NULL,
 	"name"          VARCHAR(255) NOT NULL,
 	"description"   VARCHAR(255),
 	"creation_time" TIMESTAMP NOT NULL,
 	"salt"          VARCHAR(255) NOT NULL,
 	"iterations"    VARCHAR(255) NOT NULL,
 	"masterkey"     VARCHAR(255) NOT NULL,
-	CONSTRAINT "PK_VAULT" PRIMARY KEY ("id"),
-	CONSTRAINT "FK_VAULT_ON_USER" FOREIGN KEY ("user_id") REFERENCES "user" ("id"),
-	CONSTRAINT "UNIQUE_VAULT_NAME" UNIQUE ("name")
+	CONSTRAINT "VAULT_PK" PRIMARY KEY ("id"),
+	CONSTRAINT "VAULT_FK_OWNER" FOREIGN KEY ("owner", "owner_type") REFERENCES "authority" ("id", "type"),
+	CONSTRAINT "VAULT_UNIQUE_NAME" UNIQUE ("name"),
+	CONSTRAINT "VAULT_CHK_OWNER_TYPE" CHECK ("owner_type" = 'user')
 );
 
-CREATE TABLE "vault_user"
+CREATE TABLE "vault_access"
 (
-	"vault_id"   VARCHAR(255) NOT NULL,
-	"user_id"    VARCHAR(255) NOT NULL,
-	CONSTRAINT "PK_VAULT_USER" PRIMARY KEY ("vault_id", "user_id"),
-	CONSTRAINT "FK_VAULT_USER_ON_VAULT" FOREIGN KEY ("vault_id") REFERENCES "vault" ("id") ON DELETE CASCADE,
-	CONSTRAINT "FK_VAULT_USER_ON_USER" FOREIGN KEY ("user_id") REFERENCES "user" ("id") ON DELETE CASCADE
+	"vault_id"       VARCHAR(255) NOT NULL,
+	"authority_id"   VARCHAR(255) NOT NULL,
+	"authority_type" VARCHAR(5) NOT NULL,
+	CONSTRAINT "VAULT_ACCESS_PK" PRIMARY KEY ("vault_id", "authority_id", "authority_type"),
+	CONSTRAINT "VAULT_ACCESS_FK_VAULT" FOREIGN KEY ("vault_id") REFERENCES "vault" ("id") ON DELETE CASCADE,
+	CONSTRAINT "VAULT_ACCESS_FK_AUTHORITY" FOREIGN KEY ("authority_id", "authority_type") REFERENCES "authority" ("id", "type") ON DELETE CASCADE
 );
 
 CREATE TABLE "device"
 (
 	"id"        VARCHAR(255) NOT NULL,
 	"user_id"   VARCHAR(255) NOT NULL,
+	"user_type" VARCHAR(5) NOT NULL,
 	"name"      VARCHAR(255) NOT NULL,
 	"publickey" VARCHAR(255) NOT NULL,
-	CONSTRAINT "PK_DEVICE" PRIMARY KEY ("id"),
-	CONSTRAINT "FK_DEVICE_ON_USER" FOREIGN KEY ("user_id") REFERENCES "user" ("id"),
-	CONSTRAINT "UNIQUE_DEVICE_NAME_PER_USER" UNIQUE ("user_id", "name")
+	CONSTRAINT "DEVICE_PK" PRIMARY KEY ("id"),
+	CONSTRAINT "DEVICE_FK_USER" FOREIGN KEY ("user_id", "user_type") REFERENCES "authority" ("id", "type"),
+	CONSTRAINT "DEVICE_UNIQUE_NAME_PER_USER" UNIQUE ("user_id", "user_type", "name"),
+	CONSTRAINT "DEVICE_CHK_USER_TYPE" CHECK ("user_type" = 'user')
 );
 
 CREATE TABLE "access"
 (
 	"device_id" VARCHAR(255) NOT NULL,
 	"user_id"   VARCHAR(255) NOT NULL,
+	"user_type" VARCHAR(5) NOT NULL,
 	"vault_id"  VARCHAR(255) NOT NULL,
 	"jwe"       VARCHAR(2000) NOT NULL UNIQUE,
-	CONSTRAINT "PK_ACCESS" PRIMARY KEY ("device_id", "user_id", "vault_id"),
-	CONSTRAINT "FK_ACCESS_ON_DEVICE" FOREIGN KEY ("device_id") REFERENCES "device" ("id") ON DELETE CASCADE,
-	CONSTRAINT "FK_ACCESS_ON_VAULT_USER" FOREIGN KEY ("vault_id", "user_id") REFERENCES "vault_user" ("vault_id", "user_id") ON DELETE CASCADE
+	CONSTRAINT "ACCESS_PK" PRIMARY KEY ("device_id", "user_id", "vault_id"),
+	CONSTRAINT "ACCESS_FK_DEVICE" FOREIGN KEY ("device_id") REFERENCES "device" ("id") ON DELETE CASCADE,
+	CONSTRAINT "ACCESS_FK_VAULT_ACCESS" FOREIGN KEY ("vault_id", "user_id", "user_type") REFERENCES "vault_access" ("vault_id", "authority_id", "authority_type") ON DELETE CASCADE,
+	CONSTRAINT "ACCESS_CHK_USER_TYPE" CHECK ("user_type" = 'user')
 );
