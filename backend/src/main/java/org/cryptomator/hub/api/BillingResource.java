4928a03be94a34c14f724824d53b9d62151f1f39
Armin Schrenk <armin.schrenk@skymatic.de>
2022-06-01 17:05:14 +0200

    cache license in license holder class and only query db if not set

--- backend/src/main/java/org/cryptomator/hub/api/BillingResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/BillingResource.java
@@ -1,9 +1,11 @@
 package org.cryptomator.hub.api;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.cryptomator.hub.entities.Billing;
+import org.cryptomator.hub.license.LicenseHolder;
 import org.cryptomator.hub.license.LicenseValidator;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
@@ -19,12 +21,16 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Date;
+import java.util.Optional;
 
 @Path("/billing")
 public class BillingResource {
 
 	@Inject
 	LicenseValidator licenseValidator;
+
+	@Inject
+	LicenseHolder licenseHolder;
 
 	@GET
 	@Path("/")
@@ -35,7 +41,12 @@ public class BillingResource {
 	@APIResponse(responseCode = "200")
 	@APIResponse(responseCode = "403", description = "only admins are allowed to get the billing information")
 	public BillingDto get() {
-		return Billing.<Billing>findByIdOptional(0).map(BillingDto::fromEntity).get();
+		return Optional.ofNullable(licenseHolder.get())
+				.map(BillingDto::fromDecodedJwt)
+				.orElseGet(() -> {
+					var hubId = Billing.<Billing>findAll().firstResult().hubId;
+					return BillingDto.create(hubId);
+				});
 	}
 
 	@PUT
@@ -48,15 +59,17 @@ public class BillingResource {
 	@APIResponse(responseCode = "400", description = "token is invalid (e.g., expired or invalid signature)")
 	@APIResponse(responseCode = "403", description = "only admins are allowed to set the token")
 	public Response setToken(String token) {
-		var billing = Billing.<Billing>findByIdOptional(0).get();
+		var billing = Billing.<Billing>findAll().firstResult();
 		try {
-			licenseValidator.validate(token, billing.hubId);
+			var license = licenseValidator.validate(token, billing.hubId);
+			licenseHolder.set(license);
+
+			billing.token = token;
+			billing.persist();
+			return Response.status(Response.Status.NO_CONTENT).build();
 		} catch (JWTVerificationException e) {
 			return Response.status(Response.Status.BAD_REQUEST).build();
 		}
-		billing.token = token;
-		billing.persist();
-		return Response.status(Response.Status.NO_CONTENT).build();
 	}
 
 	public record BillingDto(@JsonProperty("hubId") String hubId, @JsonProperty("hasLicense") Boolean hasLicense, @JsonProperty("email") String email,
@@ -64,18 +77,18 @@ public class BillingResource {
 							 @JsonProperty("issuedAt") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX") Date issuedAt,
 							 @JsonProperty("expiresAt") @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSX") Date expiresAt) {
 
-		public static BillingDto fromEntity(Billing entity) {
-			if (entity.token == null) {
-				return new BillingDto(entity.hubId, false, null, null, null, null, null);
-			}
-			var licenseValidator = new LicenseValidator();
-			var jwt = licenseValidator.validate(entity.token, entity.hubId);
+		public static BillingDto create(String hubId) {
+			return new BillingDto(hubId, false, null, null, null, null, null);
+		}
+
+		public static BillingDto fromDecodedJwt(DecodedJWT jwt) {
+			var id = jwt.getId();
 			var email = jwt.getSubject();
 			var totalSeats = jwt.getClaim("seats").asInt();
 			var remainingSeats = totalSeats; // TODO
 			var issuedAt = jwt.getIssuedAt();
 			var expiresAt = jwt.getExpiresAt();
-			return new BillingDto(entity.hubId, true, email, totalSeats, remainingSeats, issuedAt, expiresAt);
+			return new BillingDto(id, true, email, totalSeats, remainingSeats, issuedAt, expiresAt);
 		}
 
 	}
--- backend/src/main/java/org/cryptomator/hub/license/LicenseHolder.java
+++ backend/src/main/java/org/cryptomator/hub/license/LicenseHolder.java
@@ -1,16 +1,33 @@
 package org.cryptomator.hub.license;
 
+import com.auth0.jwt.interfaces.DecodedJWT;
+
 import javax.enterprise.context.ApplicationScoped;
+import java.time.Instant;
 import java.util.Collection;
 import java.util.EnumSet;
+import java.util.Objects;
 
 @ApplicationScoped
 public class LicenseHolder {
-
+	private volatile DecodedJWT license;
 	// TODO this is just a stub
 
+	public LicenseHolder() {
+
+	}
+
+	public void set(DecodedJWT validatedToken) {
+		Objects.requireNonNull(validatedToken);
+		this.license = validatedToken;
+	}
+
+	public DecodedJWT get() {
+		return license;
+	}
+
 	public boolean isExpired() {
-		return false;
+		return license.getExpiresAt().toInstant().isBefore(Instant.now());
 	}
 
 	public Collection<LicenseClaim> getClaims() {
