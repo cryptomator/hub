8efd5f149dda42af941bd7703d718a96ce463d1d
Armin Schrenk <armin.schrenk@skymatic.de>
2022-06-08 12:30:32 +0200

    remove unused instance var

--- backend/src/main/java/org/cryptomator/hub/license/SeatsRestrictionFilter.java
+++ backend/src/main/java/org/cryptomator/hub/license/SeatsRestrictionFilter.java
@@ -5,8 +5,6 @@ import org.cryptomator.hub.entities.User;
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
-import javax.ws.rs.container.ResourceInfo;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
@@ -20,9 +18,6 @@ public class SeatsRestrictionFilter implements ContainerRequestFilter {
 
 	@Inject
 	LicenseHolder license;
-
-	@Context
-	ResourceInfo resourceInfo;
 
 	@Override
 	public void filter(ContainerRequestContext requestContext) throws IOException {
