c61a4ca98efe82087a203abbff2dd1b8ae12ea50
Armin Schrenk <armin.schrenk@skymatic.de>
2022-07-20 15:41:55 +0200

    Adjust design of empty device list to the empty vault list

--- frontend/src/components/DeviceList.vue
+++ frontend/src/components/DeviceList.vue
@@ -8,8 +8,12 @@
     </div>
   </div>
 
-  <div v-else-if="me.devices.length == 0">
-    {{ t('deviceList.notFound') }}
+  <div v-else-if="me.devices.length == 0" class="text-center">
+    <svg xmlns="http://www.w3.org/2000/svg" class="mx-auto h-12 w-12 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+      <path vector-effect="non-scaling-stroke" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+    </svg>
+    <h3 class="mt-2 text-sm font-medium text-gray-900">{{ t('deviceList.empty.message') }}</h3>
+    <p class="mt-1 text-sm text-gray-500">{{ t('deviceList.empty.description') }}</p>
   </div>
 
   <div v-else>
