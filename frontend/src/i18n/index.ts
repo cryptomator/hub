1ca7c4e27cadd3898c75fc6b3ce76353ed6e2d1e
Julian Raufelder <Julian@Raufelder.com>
2022-07-26 18:28:52 +0200

    Use vite-plugin-vue-i18n to fix eval ("script-src") CSP violation

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -1,16 +1,9 @@
 import { I18nOptions } from 'vue-i18n';
-import de from './de-DE.json';
-import en from './en-US.json';
 
 export enum Locale {
   EN = 'en',
   DE = 'de'
 }
-
-export const messages = {
-  [Locale.EN]: en,
-  [Locale.DE]: de
-};
 
 export const datetimeFormats: I18nOptions['datetimeFormats'] = {
   [Locale.EN]: {
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,14 +2,21 @@ import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import './css/fonts.css';
-import { datetimeFormats, defaultLocale, messages } from './i18n/index';
+import { datetimeFormats, defaultLocale } from './i18n/index';
 import './index.css';
 import router from './router';
+
+// migrate to // import messages from '@intlify/vite-plugin-vue-i18n/messages'; as soon as it works
+import de from './i18n/de-DE.json';
+import en from './i18n/en-US.json';
 
 const i18n = createI18n({
   locale: defaultLocale,
   fallbackLocale: defaultLocale,
-  messages,
+  messages: {
+    de,
+    en
+  },
   datetimeFormats,
   globalInjection: true
 });
