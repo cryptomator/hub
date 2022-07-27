a4e22efe0d7d6b23ffb89c7441bf8d9fcff23297
Julian Raufelder <Julian@Raufelder.com>
2022-07-27 08:41:48 +0200

    Apply suggestions from code review

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,21 +2,15 @@ import { createApp } from 'vue';
 import { createI18n } from 'vue-i18n';
 import App from './App.vue';
 import './css/fonts.css';
-import { datetimeFormats, defaultLocale } from './i18n/index';
+import { datetimeFormats, defaultLocale, messages } from './i18n/index';
 import './index.css';
 import router from './router';
-
 // migrate to // import messages from '@intlify/vite-plugin-vue-i18n/messages'; as soon as it works
-import de from './i18n/de-DE.json';
-import en from './i18n/en-US.json';
 
 const i18n = createI18n({
   locale: defaultLocale,
   fallbackLocale: defaultLocale,
-  messages: {
-    de,
-    en
-  },
+  messages,
   datetimeFormats,
   globalInjection: true
 });
