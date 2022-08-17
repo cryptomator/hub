eb4840c26fb577627d0ac67e1f6611dfd2b2b0e5
Armin Schrenk <armin.schrenk@skymatic.de>
2022-08-17 12:24:54 +0200

    fix import errors and spacing

--- frontend/src/components/AdminSettings.vue
+++ frontend/src/components/AdminSettings.vue
@@ -67,130 +67,131 @@
           </div>
         </div>
       </div>
-    </div>
 
-    <div v-if="admin.hasLicense" class="shadow sm:rounded-lg sm:overflow-hidden">
-      <div class="bg-white px-4 py-5 sm:p-6">
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="md:col-span-1">
-            <h3 class="text-lg font-medium leading-6 text-gray-900">
-              {{ t('admin.licenseInfo.title') }}
-            </h3>
-            <p class="mt-1 text-sm text-gray-500">
-              {{ t('admin.licenseInfo.description') }}
-            </p>
-          </div>
-          <div class="mt-5 md:mt-0 md:col-span-2">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
-                <label for="email" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.email.title') }}</label>
-                <input id="email" v-model="admin.email" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" readonly />
-              </div>
+      <div v-if="admin.hasLicense" class="shadow sm:rounded-lg sm:overflow-hidden">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+              <h3 class="text-lg font-medium leading-6 text-gray-900">
+                {{ t('admin.licenseInfo.title') }}
+              </h3>
+              <p class="mt-1 text-sm text-gray-500">
+                {{ t('admin.licenseInfo.description') }}
+              </p>
+            </div>
+            <div class="mt-5 md:mt-0 md:col-span-2">
+              <div class="grid grid-cols-6 gap-6">
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="email" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.email.title') }}</label>
+                  <input id="email" v-model="admin.email" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" readonly />
+                </div>
 
-              <div class="col-span-6 sm:col-span-3">
-                <label for="seats" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.seats.title') }}</label>
-                <input id="seats" v-model="admin.totalSeats" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" aria-describedby="seats-description" readonly />
-                <p v-if="admin.remainingSeats > 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <CheckIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.seats.description.enoughSeats', [admin.remainingSeats]) }}
-                </p>
-                <p v-else-if="admin.remainingSeats == 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <ExclamationIcon class="shrink-0 text-orange-500 mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.seats.description.zeroSeats') }}
-                </p>
-                <p v-else id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <XIcon class="shrink-0 text-red-500 mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.seats.description.undercutSeats') }}
-                </p>
-              </div>
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="seats" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.seats.title') }}</label>
+                  <input id="seats" v-model="admin.totalSeats" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" aria-describedby="seats-description" readonly />
+                  <p v-if="admin.remainingSeats > 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <CheckIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.seats.description.enoughSeats', [admin.remainingSeats]) }}
+                  </p>
+                  <p v-else-if="admin.remainingSeats == 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <ExclamationIcon class="shrink-0 text-orange-500 mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.seats.description.zeroSeats') }}
+                  </p>
+                  <p v-else id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <XIcon class="shrink-0 text-red-500 mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.seats.description.undercutSeats') }}
+                  </p>
+                </div>
 
-              <div class="col-span-6 sm:col-span-3">
-                <label for="issuedAt" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.issuedAt.title') }}</label>
-                <input id="issuedAt" :value="d(admin.issuedAt, 'short')" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" readonly />
-              </div>
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="issuedAt" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.issuedAt.title') }}</label>
+                  <input id="issuedAt" :value="d(admin.issuedAt, 'short')" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" readonly />
+                </div>
 
-              <div class="col-span-6 sm:col-span-3">
-                <label for="expiresAt" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.expiresAt.title') }}</label>
-                <input id="expiresAt" :value="d(admin.expiresAt, 'short')" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" aria-describedby="expiresAt-description" readonly />
-                <p v-if="now < admin.expiresAt" id="expiresAt-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <CheckIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.expiresAt.description.valid') }}
-                </p>
-                <p v-else id="expiresAt-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <XIcon class="shrink-0 text-red-500 mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.expiresAt.description.expired') }}
-                </p>
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="expiresAt" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.expiresAt.title') }}</label>
+                  <input id="expiresAt" :value="d(admin.expiresAt, 'short')" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" aria-describedby="expiresAt-description" readonly />
+                  <p v-if="now < admin.expiresAt" id="expiresAt-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <CheckIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.expiresAt.description.valid') }}
+                  </p>
+                  <p v-else id="expiresAt-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <XIcon class="shrink-0 text-red-500 mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.expiresAt.description.expired') }}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
+
+        <div class="flex justify-end items-center px-4 py-3 bg-gray-50 sm:px-6">
+          <button type="button" class="flex-none inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 disabled:hover:bg-primary disabled:cursor-not-allowed" @click="manageSubscription()">
+            <ExternalLinkIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            {{ t('admin.licenseInfo.manageSubscription') }}
+          </button>
+        </div>
       </div>
 
-      <div class="flex justify-end items-center px-4 py-3 bg-gray-50 sm:px-6">
-        <button type="button" class="flex-none inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 disabled:hover:bg-primary disabled:cursor-not-allowed" @click="manageSubscription()">
-          <ExternalLinkIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-          {{ t('admin.licenseInfo.manageSubscription') }}
-        </button>
-      </div>
-    </div>
+      <div v-else-if="!admin.hasLicense" class="shadow sm:rounded-lg sm:overflow-hidden">
+        <div class="bg-white px-4 py-5 sm:p-6">
+          <div class="md:grid md:grid-cols-3 md:gap-6">
+            <div class="md:col-span-1">
+              <h3 class="text-lg font-medium leading-6 text-gray-900">
+                {{ t('admin.licenseInfo.title') }}
+              </h3>
+              <p class="mt-1 text-sm text-gray-500">
+                {{ t('admin.licenseInfo.communityLicense.description') }}
+              </p>
+            </div>
+            <div class="mt-5 md:mt-0 md:col-span-2">
+              <div class="grid grid-cols-6 gap-6">
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="licenseType" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.communityLicense.type.title') }}</label>
+                  <input id="licenseType" value="Community License" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" readonly />
+                </div>
 
-    <div v-else-if="!admin.hasLicense" class="shadow sm:rounded-lg sm:overflow-hidden">
-      <div class="bg-white px-4 py-5 sm:p-6">
-        <div class="md:grid md:grid-cols-3 md:gap-6">
-          <div class="md:col-span-1">
-            <h3 class="text-lg font-medium leading-6 text-gray-900">
-              {{ t('admin.licenseInfo.title') }}
-            </h3>
-            <p class="mt-1 text-sm text-gray-500">
-              {{ t('admin.licenseInfo.communityLicense.description') }}
-            </p>
-          </div>
-          <div class="mt-5 md:mt-0 md:col-span-2">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
-                <label for="licenseType" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.communityLicense.type.title') }}</label>
-                <input id="licenseType" value="Community License" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" readonly />
-              </div>
-
-              <div class="col-span-6 sm:col-span-3">
-                <label for="seats" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.seats.title') }}</label>
-                <input id="seats" v-model="admin.totalSeats" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" aria-describedby="seats-description" readonly />
-                <p v-if="admin.remainingSeats > 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <CheckIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.seats.description.enoughSeats', [admin.remainingSeats]) }}
-                </p>
-                <p v-else-if="admin.remainingSeats == 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <ExclamationIcon class="shrink-0 text-orange-500 mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.seats.description.zeroSeats') }}
-                </p>
-                <p v-else id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
-                  <XIcon class="shrink-0 text-red-500 mr-1 h-5 w-5" aria-hidden="true" />
-                  {{ t('admin.licenseInfo.seats.description.undercutSeats') }}
-                </p>
+                <div class="col-span-6 sm:col-span-3">
+                  <label for="seats" class="block text-sm font-medium text-gray-700">{{ t('admin.licenseInfo.seats.title') }}</label>
+                  <input id="seats" v-model="admin.totalSeats" type="text" class="mt-1 focus:ring-primary focus:border-primary block w-full shadow-sm sm:text-sm border-gray-300 rounded-md bg-gray-200" aria-describedby="seats-description" readonly />
+                  <p v-if="admin.remainingSeats > 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <CheckIcon class="shrink-0 text-primary mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.seats.description.enoughSeats', [admin.remainingSeats]) }}
+                  </p>
+                  <p v-else-if="admin.remainingSeats == 0" id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <ExclamationIcon class="shrink-0 text-orange-500 mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.seats.description.zeroSeats') }}
+                  </p>
+                  <p v-else id="seats-description" class="inline-flex mt-2 text-sm text-gray-500">
+                    <XIcon class="shrink-0 text-red-500 mr-1 h-5 w-5" aria-hidden="true" />
+                    {{ t('admin.licenseInfo.seats.description.undercutSeats') }}
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
 
-      <div class="flex justify-end items-center px-4 py-3 bg-gray-50 sm:px-6">
-        <button type="button" class="flex-none inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 disabled:hover:bg-primary disabled:cursor-not-allowed" @click="manageSubscription()">
-          <ExternalLinkIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
-          {{ t('admin.licenseInfo.communityLicense.upgradeLicense') }}
-        </button>
+        <div class="flex justify-end items-center px-4 py-3 bg-gray-50 sm:px-6">
+          <button type="button" class="flex-none inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-primary hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary disabled:opacity-50 disabled:hover:bg-primary disabled:cursor-not-allowed" @click="manageSubscription()">
+            <ExternalLinkIcon class="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+            {{ t('admin.licenseInfo.communityLicense.upgradeLicense') }}
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { CheckIcon, ExclamationIcon, ExternalLinkIcon, XIcon } from '@heroicons/vue/solid';
+import { CheckIcon, ExclamationIcon, ExternalLinkIcon, InformationCircleIcon, XIcon } from '@heroicons/vue/solid';
 import semver from 'semver';
 import { computed, onMounted, ref } from 'vue';
 import { useI18n } from 'vue-i18n';
 import backend, { BillingDto, VersionDto } from '../common/backend';
 import { frontendBaseURL } from '../common/config';
 import { FetchUpdateError, LatestVersionDto, updateChecker } from '../common/updatecheck';
+import FetchError from './FetchError.vue';
 
 const { t, d, locale } = useI18n({ useScope: 'global' });
 
