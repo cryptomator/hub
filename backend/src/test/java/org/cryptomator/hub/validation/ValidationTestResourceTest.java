1461d1528b523955a86e7ecaac1261c5fe6d369b
Armin Schrenk <armin.schrenk@skymatic.de>
2022-09-29 12:18:21 +0200

    Always test some malicous strings

--- backend/src/test/java/org/cryptomator/hub/validation/ValidationTestResourceTest.java
+++ backend/src/test/java/org/cryptomator/hub/validation/ValidationTestResourceTest.java
@@ -1,12 +1,18 @@
 package org.cryptomator.hub.validation;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static io.restassured.RestAssured.when;
 
@@ -36,6 +42,7 @@ public class ValidationTestResourceTest {
 		@DisplayName("Invalid uuids are rejected")
 		@ParameterizedTest
 		@ValueSource(strings = {"2G$5If2-e28b-4aTd-9ff5-8d81e6ae710", "23af23-23bc-8231", "2fa854c2-e289-4a4d-9cf5-8dd81e6ae710-23008b"})
+		@ArgumentsSource(MalicousStringsProvider.class)
 		public void testUUIDinvalid(String toTest) {
 			when().get("/test/validuuid/{uuid}", toTest)
 					.then().statusCode(400);
@@ -56,7 +63,8 @@ public class ValidationTestResourceTest {
 
 		@DisplayName("Invalid ids are rejected")
 		@ParameterizedTest
-		@ValueSource(strings = {"WHITE SPACE", "*$%"})
+		@ValueSource(strings = {"WHITE SPACE", "\u5207ä="})
+		@ArgumentsSource(MalicousStringsProvider.class)
 		public void testIdInvalid(String toTest) {
 			when().get("/test/validid/{id}", toTest)
 					.then().statusCode(400);
@@ -78,10 +86,18 @@ public class ValidationTestResourceTest {
 		@DisplayName("Invalid pseudo-base64 strings are rejected")
 		@ParameterizedTest
 		@ValueSource(strings = {"\u5207ä=", "abc==abc", "==="})
+		@ArgumentsSource(MalicousStringsProvider.class)
 		public void testPseudoBase64Invalid(String toTest) {
 			when().get("/test/validpseudobase64/{pb64String}", toTest)
 					.then().statusCode(400);
 		}
 	}
 
+	static class MalicousStringsProvider implements ArgumentsProvider {
+
+		@Override
+		public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+			return Arrays.stream(MALICOUS_STRINGS).map(Arguments::of);
+		}
+	}
 }
