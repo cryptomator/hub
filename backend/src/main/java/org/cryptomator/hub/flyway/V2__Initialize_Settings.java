b0a339ae618960eda945293dc25f6f33f74ccbec
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2023-03-06 15:26:54 +0100

    add javadoc

--- backend/src/main/java/org/cryptomator/hub/flyway/V2__Initialize_Settings.java
+++ backend/src/main/java/org/cryptomator/hub/flyway/V2__Initialize_Settings.java
@@ -3,6 +3,10 @@ package org.cryptomator.hub.flyway;
 import org.flywaydb.core.api.migration.BaseJavaMigration;
 import org.flywaydb.core.api.migration.Context;
 
+/**
+ * @deprecated This used to generated the hub id. It got replaced by <code>V3__Initialize_Settings.sql</code>, though. Despite being dead code,
+ * this class must remain present in order for Flyway to work correctly on existing installations. May be removed when we start with a new <a href="https://flywaydb.org/documentation/concepts/baselinemigrations">baseline migration</a>
+ */
 @Deprecated
 public class V2__Initialize_Settings extends BaseJavaMigration {
 
