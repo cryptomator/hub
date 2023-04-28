4599fe07b72b8010c5a0b1f6b3a8a4a3b564de56
Tobias Hagemann <tobias.hagemann@skymatic.de>
2023-04-28 15:48:14 +0200

    fixed build errors by migrating to jakarta

--- backend/src/main/java/org/cryptomator/hub/entities/AuditEvent.java
+++ backend/src/main/java/org/cryptomator/hub/entities/AuditEvent.java
@@ -2,15 +2,15 @@ package org.cryptomator.hub.entities;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.panache.common.Parameters;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorColumn;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Inheritance;
-import javax.persistence.InheritanceType;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
 import java.time.Instant;
 import java.util.Objects;
 import java.util.UUID;
--- backend/src/main/java/org/cryptomator/hub/entities/UnlockEvent.java
+++ backend/src/main/java/org/cryptomator/hub/entities/UnlockEvent.java
@@ -1,11 +1,12 @@
 package org.cryptomator.hub.entities;
 
-import javax.persistence.Column;
-import javax.persistence.DiscriminatorValue;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+
 import java.time.Instant;
 import java.util.Objects;
 import java.util.UUID;
