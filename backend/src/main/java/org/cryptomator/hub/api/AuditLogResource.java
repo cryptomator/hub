0317eca0b023d331a2c67b31bac48c148bc0be2c
Armin Schrenk <armin.schrenk@skymatic.de>
2023-05-12 13:34:27 +0200

    use a default value for page size in auditlog resource

--- backend/src/main/java/org/cryptomator/hub/api/AuditLogResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/AuditLogResource.java
@@ -4,10 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import jakarta.annotation.security.RolesAllowed;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.cryptomator.hub.entities.AuditEvent;
 import org.cryptomator.hub.entities.UnlockEvent;
@@ -31,7 +28,7 @@ public class AuditLogResource {
 	@Parameter(name = "endDate", description = "the end date of the period as ISO 8601 datetime string", in = ParameterIn.QUERY)
 	@Parameter(name = "after", description = "the id of the last entry of the previous page", in = ParameterIn.QUERY)
 	@Parameter(name = "pageSize", description = "the maximum number of entries to return", in = ParameterIn.QUERY)
-	public EventList getAllEvents(@QueryParam("startDate") Instant startDate, @QueryParam("endDate") Instant endDate, @QueryParam("after") long after, @QueryParam("pageSize") int pageSize) {
+	public EventList getAllEvents(@QueryParam("startDate") Instant startDate, @QueryParam("endDate") Instant endDate, @QueryParam("after") long after, @QueryParam("pageSize") @DefaultValue("20") int pageSize) {
 		var events = AuditEvent.findAllInPeriod(startDate, endDate, after, pageSize).map(AuditEventDto::fromEntity).toList();
 		return new EventList(events);
 	}
