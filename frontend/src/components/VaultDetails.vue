5eaaa96639e9ce9d860439ab7c9ccff4ce2bfb49
Armin Schrenk <armin.schrenk@skymatic.de>
2023-06-14 22:14:52 +0200

    change TODO

--- frontend/src/components/VaultDetails.vue
+++ frontend/src/components/VaultDetails.vue
@@ -21,7 +21,7 @@
       <div class="mt-2 flex items-center justify-between">
         <p v-if="vault != null && vault.description.length > 0" class="text-sm text-gray-500">{{ vault.description }}</p>
         <p v-else class="text-sm text-gray-500 italic">{{ t('vaultDetails.description.empty') }}</p>
-        <!-- TODO: add rest API to change vault metadata in backend
+        <!-- TODO: implement calls to change metadata (see backend.ts -> VaultService - update()) 
         <button v-if="isVaultAdmin" type="button" class="-mr-2 h-8 w-8 bg-white rounded-full flex items-center justify-center text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-primary">
           <PencilIcon class="h-5 w-5" aria-hidden="true" />
           <span class="sr-only">Add description</span>
