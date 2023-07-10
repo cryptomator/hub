c8cfd1efb815e50f5bd4f190d93a182139753c91
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2023-07-10 14:57:54 +0200

    add VaultRole filter

---
+++ backend/src/main/java/org/cryptomator/hub/filters/VaultRole.java
@@ -0,0 +1,29 @@
+package org.cryptomator.hub.filters;
+
+import jakarta.ws.rs.NameBinding;
+import org.cryptomator.hub.entities.VaultAccess;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to add the {@link VaultRoleFilter} request filter to annotated service.
+ */
+@NameBinding
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(value = RetentionPolicy.RUNTIME)
+@interface VaultRole {
+	String DEFAULT_VAULT_ID_PARAM = "vaultId";
+
+	/**
+	 * @return Roles required to access the annotated resource. Access is granted if _any_ role is present.
+	 */
+	VaultAccess.Role[] value() default { VaultAccess.Role.MEMBER };
+
+	/**
+	 * @return Name of the path parameter containing the {@link org.cryptomator.hub.entities.Vault#id vault id}
+	 */
+	String vaultIdParam() default DEFAULT_VAULT_ID_PARAM;
+}
---
+++ backend/src/test/java/org/cryptomator/hub/filters/VaultRoleFilterTest.java
@@ -0,0 +1,90 @@
+package org.cryptomator.hub.filters;
+
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.annotation.Nullable;
+import jakarta.ws.rs.ForbiddenException;
+import jakarta.ws.rs.NotAuthorizedException;
+import jakarta.ws.rs.container.ContainerRequestContext;
+import jakarta.ws.rs.container.ResourceInfo;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
+import jakarta.ws.rs.core.UriInfo;
+import org.cryptomator.hub.entities.VaultAccess;
+import org.eclipse.microprofile.jwt.JsonWebToken;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+@QuarkusTest
+public class VaultRoleFilterTest {
+
+	ResourceInfo resourceInfo = Mockito.mock(ResourceInfo.class);
+	UriInfo uriInfo = Mockito.mock(UriInfo.class);
+	ContainerRequestContext context = Mockito.mock(ContainerRequestContext.class);
+	JsonWebToken jwt = Mockito.mock(JsonWebToken.class);
+	VaultRoleFilter filter = new VaultRoleFilter();
+
+	@BeforeEach
+	public void setup() {
+		filter.resourceInfo = resourceInfo;
+		filter.jwt = jwt;
+
+		Mockito.doReturn(uriInfo).when(context).getUriInfo();
+	}
+
+	@Test
+	@DisplayName("error 403 if annotated resource has no vaultId path param")
+	public void testFilterWithMissingVaultId() throws NoSuchMethodException {
+		Mockito.doReturn(getClass().getMethod("allowMember")).when(resourceInfo).getResourceMethod();
+		Mockito.doReturn(new MultivaluedHashMap<>()).when(uriInfo).getPathParameters();
+
+		Assertions.assertThrows(ForbiddenException.class, () -> filter.filter(context));
+	}
+
+	@Test
+	@DisplayName("error 401 if JWT is missing")
+	public void testFilterWithMissingJWT() throws NoSuchMethodException {
+		Mockito.doReturn(getClass().getMethod("allowMember")).when(resourceInfo).getResourceMethod();
+		Mockito.doReturn(new MultivaluedHashMap<>(Map.of(VaultRole.DEFAULT_VAULT_ID_PARAM, "7E57C0DE-0000-4000-8000-000100001111"))).when(uriInfo).getPathParameters();
+
+		Assertions.assertThrows(NotAuthorizedException.class, () -> filter.filter(context));
+	}
+
+	@Test
+	@DisplayName("error 403 if user2 tries to access 7E57C0DE-0000-4000-8000-000100001111")
+	public void testFilterWithInsufficientPrivileges() throws NoSuchMethodException {
+		Mockito.doReturn(getClass().getMethod("allowOwner")).when(resourceInfo).getResourceMethod();
+		Mockito.doReturn(new MultivaluedHashMap<>(Map.of(VaultRole.DEFAULT_VAULT_ID_PARAM, "7E57C0DE-0000-4000-8000-000100001111"))).when(uriInfo).getPathParameters();
+		Mockito.doReturn("user2").when(jwt).getSubject();
+
+		var e = Assertions.assertThrows(ForbiddenException.class, () -> filter.filter(context));
+
+		Assertions.assertEquals("Vault role required: OWNER", e.getMessage());
+	}
+
+	@Test
+	@DisplayName("success if user1 tries to access 7E57C0DE-0000-4000-8000-000100001111")
+	public void testFilterSuccess() throws NoSuchMethodException {
+		Mockito.doReturn(getClass().getMethod("allowOwner")).when(resourceInfo).getResourceMethod();
+		Mockito.doReturn(new MultivaluedHashMap<>(Map.of(VaultRole.DEFAULT_VAULT_ID_PARAM, "7E57C0DE-0000-4000-8000-000100001111"))).when(uriInfo).getPathParameters();
+		Mockito.doReturn("user1").when(jwt).getSubject();
+
+		Assertions.assertDoesNotThrow(() -> filter.filter(context));
+	}
+
+	/*
+	 * "real" methods for testing below, as we can not mock Method.class without breaking Mockito
+	 */
+
+	@VaultRole({VaultAccess.Role.MEMBER})
+	public void allowMember() {}
+
+	@VaultRole({VaultAccess.Role.OWNER})
+	public void allowOwner() {}
+
+}
