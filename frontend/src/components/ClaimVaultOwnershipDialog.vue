4a701d6496f1195b6c0644efde9764b475772d09
Tobias Hagemann <tobias.hagemann@skymatic.de>
2023-08-01 16:45:45 +0200

    updated labels in claim vault ownership dialog
    [ci skip]

--- frontend/src/components/ClaimVaultOwnershipDialog.vue
+++ frontend/src/components/ClaimVaultOwnershipDialog.vue
@@ -30,7 +30,7 @@
                 </div>
                 <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse items-baseline">
                   <button type="submit" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-primary  text-base font-medium text-white hover:bg-primary-d1 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary sm:ml-3 sm:w-auto sm:text-sm">
-                    {{ t('common.manage') }}
+                    {{ t('claimVaultOwnershipDialog.submit') }}
                   </button>
                   <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm" @click="open = false">
                     {{ t('common.cancel') }}
