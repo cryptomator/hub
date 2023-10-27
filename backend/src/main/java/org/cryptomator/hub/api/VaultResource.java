e959779b5f86fefe6739fa6db7baabc142e14c89
Julian Raufelder <Julian@Raufelder.com>
2023-10-27 13:08:58 +0200

    Enhance type and name of the optional access-token ignoreArchived flag

--- backend/src/main/java/org/cryptomator/hub/api/VaultResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/VaultResource.java
@@ -312,9 +312,9 @@ public class VaultResource {
 	@APIResponse(responseCode = "404", description = "unknown vault")
 	@APIResponse(responseCode = "410", description = "Vault is archived. Only returned if ignoreArchivedVault is false or not set, otherwise the archived flag is ignored")
 	@ActiveLicense // may throw 402
-	public String unlock(@PathParam("vaultId") UUID vaultId, @QueryParam("ignoreArchivedFlag") Optional<Boolean> ignoreArchivedFlag) {
+	public String unlock(@PathParam("vaultId") UUID vaultId, @QueryParam("evenIfArchived") @DefaultValue("false") boolean ignoreArchived) {
 		var vault = Vault.<Vault>findById(vaultId); // should always be found, since @VaultRole filter would have triggered
-		if (vault.archived && !ignoreArchivedFlag.orElse(false)) {
+		if (vault.archived && !ignoreArchived) {
 			throw new GoneException("Vault is archived.");
 		}
 
--- frontend/src/common/backend.ts
+++ frontend/src/common/backend.ts
@@ -257,7 +257,7 @@ class VaultService {
   }
 
   public async accessToken(vaultId: string): Promise<string> {
-    return axiosAuth.get(`/vaults/${vaultId}/access-token?ignoreArchivedFlag=true`, { headers: { 'Content-Type': 'text/plain' } })
+    return axiosAuth.get(`/vaults/${vaultId}/access-token?evenIfArchived=true`, { headers: { 'Content-Type': 'text/plain' } })
       .then(response => response.data)
       .catch((error) => rethrowAndConvertIfExpected(error, 403));
   }
