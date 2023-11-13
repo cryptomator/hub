e577da3261d6f68d9ff51e5488ec1f484ffa544c
Julian Raufelder <Julian@Raufelder.com>
2023-11-13 13:51:27 +0100

    Add EffectiveMembers of a Group resource

--- backend/src/main/java/org/cryptomator/hub/api/GroupsResource.java
+++ backend/src/main/java/org/cryptomator/hub/api/GroupsResource.java
@@ -3,9 +3,12 @@ package org.cryptomator.hub.api;
 import jakarta.annotation.security.RolesAllowed;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.cryptomator.hub.entities.EffectiveGroupMembership;
 import org.cryptomator.hub.entities.Group;
+import org.cryptomator.hub.validation.ValidId;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 
 import java.util.List;
@@ -21,6 +24,15 @@ public class GroupsResource {
 	@Operation(summary = "list all groups")
 	public List<GroupDto> getAll() {
 		return Group.findAll().<Group>stream().map(GroupDto::fromEntity).toList();
+	}
+
+	@GET
+	@Path("/{groupId}/effective-members")
+	@RolesAllowed("user")
+	@Produces(MediaType.APPLICATION_JSON)
+	@Operation(summary = "list all effective group members")
+	public List<UserDto> getEffectiveMembers(@PathParam("groupId") @ValidId String groupId) {
+		return EffectiveGroupMembership.getEffectiveGroupUsers(groupId).map(UserDto::justPublicInfo).toList();
 	}
 
 }
--- backend/src/test/java/org/cryptomator/hub/api/GroupsResourceTest.java
+++ backend/src/test/java/org/cryptomator/hub/api/GroupsResourceTest.java
@@ -1,10 +1,13 @@
 package org.cryptomator.hub.api;
 
+import io.agroal.api.AgroalDataSource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import io.quarkus.test.security.oidc.Claim;
 import io.quarkus.test.security.oidc.OidcSecurity;
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import jakarta.inject.Inject;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -12,6 +15,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.sql.SQLException;
+
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
@@ -40,6 +46,14 @@ public class GroupsResourceTest {
 			when().get("/groups")
 					.then().statusCode(200)
 					.body("id", hasItems("group1", "group2"));
+		}
+
+		@Test
+		@DisplayName("GET /groups/group1/effective-members contains direct and subgroup members")
+		public void testGetEffectiveUsers() {
+			when().get("/groups/{groupId}/effective-members", "group1")
+					.then().statusCode(200)
+					.body("id", hasItems("user1"));
 		}
 
 	}
