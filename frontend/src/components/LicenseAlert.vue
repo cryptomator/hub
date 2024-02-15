836fd8713b33e669be4a3c2621b735c5973db352
Julian Raufelder <Julian@Raufelder.com>
2024-02-15 13:13:15 +0100

    Enhance wording and fix colors in LicenseAlert

--- frontend/src/components/LicenseAlert.vue
+++ frontend/src/components/LicenseAlert.vue
@@ -5,8 +5,8 @@
         <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
       </div>
       <div class="ml-3">
-        <h3 class="text-sm font-medium text-red-800">{{ t('licenseAlert.title') }}</h3>
-        <i18n-t keypath="licenseAlert.licenseExpired" scope="global" tag="p" class="mt-2 text-sm text-red-700">
+        <h3 class="text-sm font-medium text-red-800">{{ t('licenseAlert.noRemainingSeats.title') }}</h3>
+        <i18n-t keypath="licenseAlert.description" scope="global" tag="p" class="mt-2 text-sm text-red-700">
           <router-link to="/app/admin/settings" class="text-sm text-red-700 underline hover:text-red-600">
             {{ t('licenseAlert.button') }}
           </router-link>
@@ -21,9 +21,9 @@
         <ExclamationTriangleIcon class="h-5 w-5 text-yellow-400" aria-hidden="true" />
       </div>
       <div class="ml-3">
-        <h3 class="text-sm font-medium text-yellow-800">{{ t('licenseAlert.title') }}</h3>
-        <i18n-t keypath="licenseAlert.noRemainingSeats" scope="global" tag="p" class="mt-2 text-sm text-red-700">
-          <router-link to="/app/admin/settings" class="text-sm text-red-700 underline hover:text-red-600">
+        <h3 class="text-sm font-medium text-yellow-800">{{ t('licenseAlert.licenseExpired.title') }}</h3>
+        <i18n-t keypath="licenseAlert.description" scope="global" tag="p" class="mt-2 text-sm text-yellow-700">
+          <router-link to="/app/admin/settings" class="text-sm text-yellow-700 underline hover:text-yellow-600">
             {{ t('licenseAlert.button') }}
           </router-link>
         </i18n-t>
