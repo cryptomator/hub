aeafffe1e741c4980afd7aface29e76dc99134ca
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2024-05-09 14:18:57 +0200

    fix mocha test explorer: `document is not defined`

--- frontend/src/common/config.ts
+++ frontend/src/common/config.ts
@@ -1,11 +1,13 @@
 import AxiosStatic from 'axios';
 
+const url = (typeof document !== 'undefined') ? new URL(document.baseURI) : new URL('http://localhost/'); // workaround for testing in Node environment
+
 // these URLs must end on '/':
-export const baseURL = new URL(document.baseURI).pathname;
+export const baseURL = url.pathname;
 export const frontendBaseURL = `${baseURL}app/`;
-export const absFrontendBaseURL = `${location.origin}${frontendBaseURL}`;
+export const absFrontendBaseURL = `${url.origin}${frontendBaseURL}`;
 export const backendBaseURL = `${baseURL}api/`;
-export const absBackendBaseURL = `${location.origin}${backendBaseURL}`;
+export const absBackendBaseURL = `${url.origin}${backendBaseURL}`;
 
 const axios = AxiosStatic.create({
   baseURL: backendBaseURL,
