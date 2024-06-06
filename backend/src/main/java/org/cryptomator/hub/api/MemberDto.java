26369a57a68093f3f0480a2dbc68fa57007e60f6
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2024-06-06 16:59:03 +0200

    adjust DTO to carry both EC keys

--- backend/src/main/java/org/cryptomator/hub/api/MemberDto.java
+++ backend/src/main/java/org/cryptomator/hub/api/MemberDto.java
@@ -7,23 +7,26 @@ import org.cryptomator.hub.entities.VaultAccess;
 
 public final class MemberDto extends AuthorityDto {
 
-	@JsonProperty("publicKey")
-	public final String publicKey;
+	@JsonProperty("ecdhPublicKey")
+	public final String ecdhPublicKey;
+	@JsonProperty("ecdsaPublicKey")
+	public final String ecdsaPublicKey;
 	@JsonProperty("role")
 	public final VaultAccess.Role role;
 
-	MemberDto(@JsonProperty("id") String id, @JsonProperty("type") Type type, @JsonProperty("name") String name, @JsonProperty("pictureUrl") String pictureUrl, @JsonProperty("publicKey") String publicKey, @JsonProperty("role") VaultAccess.Role role) {
+	MemberDto(@JsonProperty("id") String id, @JsonProperty("type") Type type, @JsonProperty("name") String name, @JsonProperty("pictureUrl") String pictureUrl, @JsonProperty("ecdhPublicKey") String ecdhPublicKey, @JsonProperty("ecdsaPublicKey") String ecdsaPublicKey, @JsonProperty("role") VaultAccess.Role role) {
 		super(id, type, name, pictureUrl);
-		this.publicKey = publicKey;
+		this.ecdhPublicKey = ecdhPublicKey;
+		this.ecdsaPublicKey = ecdsaPublicKey;
 		this.role = role;
 	}
 
 	public static MemberDto fromEntity(User user, VaultAccess.Role role) {
-		return new MemberDto(user.getId(), Type.USER, user.getName(), user.getPictureUrl(), user.getPublicKey(), role);
+		return new MemberDto(user.getId(), Type.USER, user.getName(), user.getPictureUrl(), user.getEcdhPublicKey(), user.getEcdsaPublicKey(), role);
 	}
 
 	public static MemberDto fromEntity(Group group, VaultAccess.Role role) {
-		return new MemberDto(group.getId(), Type.GROUP, group.getName(), null, null, role);
+		return new MemberDto(group.getId(), Type.GROUP, group.getName(), null, null, null, role);
 	}
 
 }
