04eb3f5dde35c87362b908c66ec968e719b911e8
Julian Raufelder <Julian@Raufelder.com>
2025-02-10 14:29:06 +0100

    Accept and export also publicKey/privateKey in UserDto
    For legacy reasons

--- backend/src/main/java/org/cryptomator/hub/api/UserDto.java
+++ backend/src/main/java/org/cryptomator/hub/api/UserDto.java
@@ -1,5 +1,6 @@
 package org.cryptomator.hub.api;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.Nullable;
 import org.cryptomator.hub.entities.User;
@@ -17,24 +18,18 @@ public final class UserDto extends AuthorityDto {
 	@JsonProperty("devices")
 	public final Set<DeviceResource.DeviceDto> devices;
 	@JsonProperty("ecdhPublicKey")
+	@JsonAlias("publicKey") // name for history reasons (don't break client compatibility)
 	public final String ecdhPublicKey;
 	@JsonProperty("ecdsaPublicKey")
 	public final String ecdsaPublicKey;
-	@JsonProperty("privateKey") // singular name for history reasons (don't break client compatibility)
+	@JsonProperty("privateKeys")
+	@JsonAlias("privateKey") // name for history reasons (don't break client compatibility)
 	public final String privateKeys;
 	@JsonProperty("setupCode")
 	public final String setupCode;
 
-	/**
-	 * Same as {@link #ecdhPublicKey}, kept for compatibility purposes
-	 * @deprecated to be removed when all clients moved to the new DTO field names
-	 */
-	@Deprecated(forRemoval = true)
-	@JsonProperty("publicKey")
-	public final String legacyEcdhPublicKey;
-
-	UserDto(@JsonProperty("id") String id, @JsonProperty("name") String name, @JsonProperty("pictureUrl") String pictureUrl, @JsonProperty("email") String email, @JsonProperty("language") String language, @JsonProperty("devices") Set<DeviceResource.DeviceDto> devices,
-			@Nullable @JsonProperty("ecdhPublicKey") @OnlyBase64Chars String ecdhPublicKey, @Nullable @JsonProperty("ecdsaPublicKey") @OnlyBase64Chars String ecdsaPublicKey, @Nullable @JsonProperty("privateKeys") @ValidJWE String privateKeys, @Nullable @JsonProperty("setupCode") @ValidJWE String setupCode) {
+	public UserDto(@JsonProperty("id") String id, @JsonProperty("name") String name, @JsonProperty("pictureUrl") String pictureUrl, @JsonProperty("email") String email, @JsonProperty("language") String language, @JsonProperty("devices") Set<DeviceResource.DeviceDto> devices,
+				   @Nullable @JsonProperty("ecdhPublicKey") @JsonAlias("publicKey") @OnlyBase64Chars String ecdhPublicKey, @Nullable @JsonProperty("ecdsaPublicKey") @OnlyBase64Chars String ecdsaPublicKey, @Nullable @JsonProperty("privateKeys") @JsonAlias("privateKey") @ValidJWE String privateKeys, @Nullable @JsonProperty("setupCode") @ValidJWE String setupCode) {
 		super(id, Type.USER, name, pictureUrl);
 		this.email = email;
 		this.language = language;
@@ -43,12 +38,29 @@ public final class UserDto extends AuthorityDto {
 		this.ecdsaPublicKey = ecdsaPublicKey;
 		this.privateKeys = privateKeys;
 		this.setupCode = setupCode;
-
-		// duplicate fields to maintain backwards compatibility:
-		this.legacyEcdhPublicKey = ecdhPublicKey;
 	}
 
 	public static UserDto justPublicInfo(User user) {
-		return new UserDto(user.getId(), user.getName(), user.getPictureUrl(), user.getEmail(), user.getLanguage(), Set.of(), user.getEcdhPublicKey(), user.getEcdsaPublicKey(),null, null);
+		return new UserDto(user.getId(), user.getName(), user.getPictureUrl(), user.getEmail(), user.getLanguage(), Set.of(), user.getEcdhPublicKey(), user.getEcdsaPublicKey(), null, null);
+	}
+
+	/**
+	 * Same as {@link #ecdhPublicKey}, kept for compatibility purposes
+	 * @deprecated to be removed when all clients moved to the new DTO field names
+	 */
+	@Deprecated(forRemoval = true)
+	@JsonProperty("publicKey")
+	public String getPublicKey() {
+		return ecdhPublicKey;
+	}
+
+	/**
+	 * Same as {@link #privateKeys}, kept for compatibility purposes
+	 * @deprecated to be removed when all clients moved to the new DTO field names
+	 */
+	@Deprecated(forRemoval = true)
+	@JsonProperty("privateKey")
+	public String getPrivateKey() {
+		return privateKeys;
 	}
 }
