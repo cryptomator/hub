fa2b89941182f5391a268f511bbeaa2e5237a720
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2025-03-10 12:09:43 +0100

    display memberSize in member search and list

--- frontend/src/components/SearchInputGroup.vue
+++ frontend/src/components/SearchInputGroup.vue
@@ -9,7 +9,12 @@
           </div>
 
           <ComboboxInput v-if="selectedItem == null" v-focus class="w-full h-10 rounded-l-md border border-gray-300 bg-white py-2 px-10 shadow-xs focus:border-primary focus:outline-hidden focus:ring-1 focus:ring-primary sm:text-sm disabled:bg-primary-l2" placeholder="John Doe" @change="query = $event.target.value"/>
-          <input v-else v-model="selectedItem.name" class="w-full h-10 rounded-l-md border border-gray-300 bg-primary-l2 py-2 px-10 shadow-xs focus:border-primary focus:outline-hidden focus:ring-1 focus:ring-primary sm:text-sm" readonly />
+          <div v-else class="w-full h-10 rounded-l-md border border-gray-300 bg-primary-l2 py-2 px-10 flex items-center justify-between shadow-xs sm:text-sm">
+            <span class="truncate">{{ selectedItem.name }}</span>
+            <span v-if="selectedItem.type === 'GROUP'" class="text-gray-500 text-xs italic mr-6">
+              {{ selectedItem.memberSize }} {{ t('vaultDetails.sharedWith.members') }}
+            </span>
+          </div>
         </div>
 
         <ComboboxOptions v-if="selectedItem == null && filteredItems.length > 0" class="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black/5 focus:outline-hidden sm:text-sm">
@@ -17,6 +22,9 @@
             <div class="flex items-center">
               <img :src="item.pictureUrl ?? ''" alt="" class="h-6 w-6 shrink-0 rounded-full" >
               <span class="ml-3 truncate">{{ item.name }}</span>
+              <span v-if="item.type === 'GROUP'" class="ml-auto text-xs text-gray-500 italic">
+                {{ item.memberSize }} {{ t('vaultDetails.sharedWith.members') }}
+              </span>
             </div>
           </ComboboxOption>
         </ComboboxOptions>
@@ -38,12 +46,17 @@ import { Combobox, ComboboxInput, ComboboxOption, ComboboxOptions } from '@headl
 import { UsersIcon, XCircleIcon } from '@heroicons/vue/24/solid';
 import { computed, nextTick, ref, shallowRef, watch } from 'vue';
 import { debounce } from '../common/util';
+import { useI18n } from 'vue-i18n';
 
 export type Item = {
   id: string;
   name: string;
   pictureUrl?: string;
+  type?: string;
+  memberSize?: number;
 }
+
+const { t } = useI18n({ useScope: 'global' });
 
 const props = defineProps<{
   actionTitle: string
