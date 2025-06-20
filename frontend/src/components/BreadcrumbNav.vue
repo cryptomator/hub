11a7b164e6b937be9d42da241e3bca09210127fd
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2025-06-20 10:49:52 +0200

    changed svg to heroicons ChevronRightIcon

--- frontend/src/components/BreadcrumbNav.vue
+++ frontend/src/components/BreadcrumbNav.vue
@@ -5,23 +5,21 @@
         <template v-if="crumb.to">
           <RouterLink :to="crumb.to" class="inline-flex items-center hover:text-gray-700">
             {{ crumb.label }}
-            <svg v-if="index < crumbs.length - 1" class="w-4 h-4 mx-1 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-              <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L11.586 9 7.293 4.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
-            </svg>
+            <ChevronRightIcon v-if="index < crumbs.length - 1" class="w-4 h-4 mx-1 text-gray-400"/>
           </RouterLink>
         </template>
         <template v-else>
           <span class="text-gray-700 font-medium">{{ crumb.label }}</span>
         </template>
-        <svg v-if="index < crumbs.length - 1 && !crumb.to" class="w-4 h-4 mx-1 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-          <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L11.586 9 7.293 4.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-        </svg>
+        <ChevronRightIcon v-if="index < crumbs.length - 1 && !crumb.to" class="w-4 h-4 mx-1 text-gray-400"/>
       </li>
     </ol>
   </nav>
 </template>
 
 <script setup>
+import { ChevronRightIcon } from '@heroicons/vue/20/solid';
+
 defineProps({
   crumbs: {
     type: Array,
