ce845b03311ff50d86432cf5cd144d16686d5632
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2025-08-05 09:43:58 +0200

    style improvement

--- frontend/src/components/VaultCreationProgress.vue
+++ frontend/src/components/VaultCreationProgress.vue
@@ -3,7 +3,7 @@
     <template v-for="(step, index) in steps" :key="step">
       <div class="flex items-center">
         <div
-          class="relative z-10 flex h-7 w-7 items-center justify-center rounded-full border-3 cursor-default"
+          class="relative z-10 flex h-8 w-8 items-center justify-center rounded-full border-2 cursor-default"
           :class="{
             'bg-primary border-primary text-white': index < currentStep,
             'bg-white border-gray-300 text-black': index > currentStep,
@@ -12,15 +12,21 @@
           :title="tooltipForStep(step)"
         >
           <template v-if="index < currentStep">
-            <CheckIcon class="text-white" />
+            <CheckIcon class="text-white m-0.5" />
           </template>
           <template v-else>
-            <span class="h-1.5 w-1.5 rounded-full block" :class="index === currentStep ? 'bg-primary' : 'bg-gray-300'"/>
+            <span
+              class="h-2.5 w-2.5 rounded-full block"
+              :class="{
+                'bg-primary opacity-100': index === currentStep,
+                'bg-gray-300 opacity-0': index > currentStep
+              }"
+            />
           </template>
         </div>
         <div
           v-if="index < steps.length - 1"
-          class="w-10 sm:w-12 h-1"
+          class="w-10 sm:w-12 h-0.5"
           :class="index < currentStep ? 'bg-primary' : 'bg-gray-300'"
         />
       </div>
