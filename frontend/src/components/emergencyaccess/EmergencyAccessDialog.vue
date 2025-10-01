a7dad3273959fa9df1fb86161992e640367ba0c4
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2025-10-01 08:27:15 +0200

    move addMyShare back to approveRecovery

--- frontend/src/components/emergencyaccess/EmergencyAccessDialog.vue
+++ frontend/src/components/emergencyaccess/EmergencyAccessDialog.vue
@@ -483,6 +483,7 @@ async function approveRecovery() {
     // add my part of the emergency key:
     const process = structuredClone(toRaw(props.recoveryProcess));
     process.recoveredKeyShares[props.me.id] = await addMyShare(process, userKeys);
+    await backend.emergencyAccess.addMyShare(process.id, process.recoveredKeyShares[props.me.id]);
 
     // done:
     emit('updated', process);
@@ -513,7 +514,6 @@ async function completeRecovery() {
     // add my part of the emergency key:
     const process = structuredClone(toRaw(props.recoveryProcess));
     process.recoveredKeyShares[props.me.id] = await addMyShare(process, userKeys);
-    await backend.emergencyAccess.addMyShare(process.id, process.recoveredKeyShares[props.me.id]);
 
     // collect key parts:
     const keyShares = Object.values(process.recoveredKeyShares).filter(p => p.recoveredKeyShare !== undefined).map(p => p.recoveredKeyShare!);
