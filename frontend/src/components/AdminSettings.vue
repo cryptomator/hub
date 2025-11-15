89f92b2dacdd230a5ca00107abe9fe839191d8ff
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2025-11-15 15:39:42 +0100

    redirect using location API

--- frontend/src/components/AdminSettings.vue
+++ frontend/src/components/AdminSettings.vue
@@ -390,7 +390,7 @@ async function fetchData() {
 
 function manageSubscription() {
   const returnUrl = `${absFrontendBaseURL}admin`;
-  window.open(`https://cryptomator.org/hub/billing/?hub_id=${billing.value?.hubId}&return_url=${encodeURIComponent(returnUrl)}`, '_self');
+  window.location.href = `https://cryptomator.org/hub/billing/?hub_id=${billing.value?.hubId}&return_url=${encodeURIComponent(returnUrl)}`;
 }
 
 async function saveWebOfTrust() {
