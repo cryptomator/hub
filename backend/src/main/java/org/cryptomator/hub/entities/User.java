0f3265539dccaa97e92ce61a5f61cdf3cab64f7e
Julian Raufelder <Julian@Raufelder.com>
2025-12-11 09:57:00 +0100

    Apply suggestions from code review

--- backend/src/main/java/org/cryptomator/hub/entities/User.java
+++ backend/src/main/java/org/cryptomator/hub/entities/User.java
@@ -157,7 +157,7 @@ public class User extends Authority {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(super.getId(), email);
+		return Objects.hash(super.getId(), email, super.getPictureUrl());
 	}
 
 	@ApplicationScoped
