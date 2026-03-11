8bba159a92041c2b04552b99ace8e44f4a7fc895
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2026-03-11 14:08:36 +0100

    add legacy device deprecation warning banner for users and admins

--- backend/src/main/java/org/cryptomator/hub/entities/LegacyDevice.java
+++ backend/src/main/java/org/cryptomator/hub/entities/LegacyDevice.java
@@ -98,6 +98,10 @@ public class LegacyDevice {
 			return find("#LegacyDevice.allInList", Parameters.with("ids", ids)).stream();
 		}
 
+		public boolean existsAny() {
+			return count() > 0;
+		}
+
 		public void deleteByOwner(String userId) {
 			delete("#LegacyDevice.deleteByOwner", Parameters.with("userId", userId));
 		}
