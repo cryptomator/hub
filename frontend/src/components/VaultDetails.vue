5d5c465caf63e1a3e16c4f91ed93beb7e4747f29
Tobias Hagemann <tobias.hagemann@skymatic.de>
2026-03-27 06:59:37 +0100

    fix archive/reactivate button visibility in licenseViolated section

--- frontend/src/components/VaultDetails.vue
+++ frontend/src/components/VaultDetails.vue
@@ -130,8 +130,12 @@
           {{ t('vaultDetails.actions.editVaultMetadata') }}
         </button>
         <!-- archiveVault button -->
-        <button v-if="canToggleArchive" type="button" class="bg-red-600 py-2 px-4 border border-transparent rounded-md shadow-xs text-sm font-medium text-white  hover:bg-red-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-red-500" @click="showArchiveVaultDialog()">
+        <button v-if="canToggleArchive && !vault.archived" type="button" class="bg-red-600 py-2 px-4 border border-transparent rounded-md shadow-xs text-sm font-medium text-white  hover:bg-red-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-red-500" @click="showArchiveVaultDialog()">
           {{ t('vaultDetails.actions.archiveVault') }}
+        </button>
+        <!-- reactivateVault button -->
+        <button v-if="canToggleArchive && vault.archived" type="button" class="bg-red-600 py-2 px-4 border border-transparent rounded-md shadow-xs text-sm font-medium text-white  hover:bg-red-700 focus:outline-hidden focus:ring-2 focus:ring-offset-2 focus:ring-red-500" @click="showReactivateVaultDialog()">
+          {{ t('vaultDetails.actions.reactivateVault') }}
         </button>
       </div>
 
