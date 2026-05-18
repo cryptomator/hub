552c8c47617e7b2385dd398d7c74cf561fbdfe3b
Jan-Peter Klein <jan-peter.klein@skymatic.de>
2026-05-18 16:55:41 +0200

    visually disable 'allow choosing keyholders' checkbox

--- frontend/src/components/AdminSettingsEmergencyAccess.vue
+++ frontend/src/components/AdminSettingsEmergencyAccess.vue
@@ -115,9 +115,9 @@
             v-model="allowChoosing"
             :disabled="!enableEmergencyAccess"
             type="checkbox"
-            class="h-4 w-4 text-primary focus:ring-primary border-gray-300 rounded"
+            class="peer h-4 w-4 text-primary focus:ring-primary border-gray-300 rounded disabled:cursor-not-allowed disabled:opacity-50"
           />
-          <label for="allow" class="ml-2 text-sm text-gray-500">
+          <label for="allow" class="ml-2 text-sm text-gray-500 peer-disabled:cursor-not-allowed peer-disabled:opacity-50">
             {{ t('admin.emergencyAccess.allowChoosing.label') }}
             <label v-if="allowChoosing" for="minMembers"> {{ t('admin.emergencyAccess.allowChoosing.atLeast') }}</label>
           </label>
