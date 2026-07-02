a78c713c4aca9b5faa8696a3564534112151d20c
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2026-07-02 13:41:17 +0200

    simplify MultiUserSelectInputGroup, fix DOM warns

--- frontend/src/components/AdminSettingsEmergencyAccess.vue
+++ frontend/src/components/AdminSettingsEmergencyAccess.vue
@@ -85,7 +85,6 @@
         <div class="mt-1 md:mt-0 lg:col-span-3 md:col-span-4">
           <div class="relative">
             <MultiUserSelectInputGroup
-              v-if="enableEmergencyAccess"
               input-id="searchKeyholder"
               :selected-users="selectedUsers"
               :on-search="searchCouncilMembers"
@@ -95,13 +94,6 @@
               :placeholder="t('common.search.placeholder')"
               @action="selectUser"
               @remove="removeUser"
-            />
-            <MultiUserSelectInputGroup
-              v-else
-              :selected-users="selectedUsers"
-              :on-search="async () => []"
-              :input-visible="enableEmergencyAccess"
-              :disable-action="true"
             />
             <p class="mt-2 text-sm text-gray-500">{{ t('admin.emergencyAccess.keyholders.help') }}</p>
           </div>
