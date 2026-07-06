48d9a95b2708d8b70d6b245d02472358151623ab
Armin Schrenk <armin.schrenk@skymatic.de>
2026-07-07 00:51:39 +0200

    [build image] Use upgradeLicenseBanner also for EA

--- frontend/src/components/AdminSettingsEmergencyAccess.vue
+++ frontend/src/components/AdminSettingsEmergencyAccess.vue
@@ -9,9 +9,7 @@
       </a>
     </p>
     <hr class="my-4 border-gray-200" />
-    <ContentBanner v-if="!entitlements.emergencyAccessEnabled" type="info" :title="t('missingEntitlements.title')">
-      {{ t('missingEntitlements.description') }} <!-- TODO: link to feature comparison? -->
-    </ContentBanner>
+    <UpgradeLicenseBanner v-if="!entitlements.emergencyAccessEnabled" />
     <form v-else class="space-y-6 md:gap-6" novalidate @submit.prevent="saveRecoverySettings">
       <ContentBanner v-if="entitlements.showTrialHint" type="info" :title="t('trial.enterpriseFeature.title')" class="mb-6">
         {{ t('trial.enterpriseFeature.description') }} <!-- TODO: link to feature comparison? -->
@@ -197,6 +195,7 @@
 import { ref, computed, onMounted, watch } from 'vue';
 import { ArrowRightIcon, ExclamationTriangleIcon } from '@heroicons/vue/20/solid';
 import ContentBanner from './ContentBanner.vue';
+import UpgradeLicenseBanner from './UpgradeLicenseBanner.vue';
 import { useI18n } from 'vue-i18n';
 import backend, { UserDto, ActivatedUser, didCompleteSetup } from '../common/backend';
 import config from '../common/config';
